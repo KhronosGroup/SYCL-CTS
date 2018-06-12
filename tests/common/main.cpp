08c53c34ccf9a196051ede025f46594d5ab5c6da
Marios Katsigiannis <marios.katsigiannis@codeplay.com>
2018-06-12 15:38:03 +0100

    fixing the copyright notice

--- tests/common/main.cpp
+++ tests/common/main.cpp
@@ -2,7 +2,7 @@
 //
 //  SYCL 1.2.1 Conformance Test Suite
 //
-//  Copyright:	(c) 2017 by Codeplay Software LTD. All Rights Reserved.
+//  Copyright:	(c) 2018 by Codeplay Software LTD. All Rights Reserved.
 //
 *******************************************************************************/
 
