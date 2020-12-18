be0dbca3d8ef94f17e917c8ff54283faef018e6d
Kopylova <nataliax.kopylova@intel.com>
2020-12-18 17:49:44 +0700

    Removed copyright from new file

--- tests/image/image_api_fp16.cpp
+++ tests/image/image_api_fp16.cpp
@@ -2,7 +2,7 @@
 //
 //  SYCL 1.2.1 Conformance Test Suite
 //
-//  Copyright:	(c) 2017 by Codeplay Software LTD. All Rights Reserved.
+//  Provides image get_access test for cl_half4
 //
 *******************************************************************************/
 
