55649cf33cc2c7195d22531b545c157f2079257e
Philip Salzmann <philip.salzmann@uibk.ac.at>
2021-04-13 11:41:34 +0200

    Add sycl = ::cl::sycl namespace alias
    SYCL 2020 introduces a new canonical namespace, ::sycl, which is defined
    within the new <sycl/sycl.hpp> header. To ease the transition of the CTS
    to the new namespace, this creates an alias `sycl = cl::sycl`, while
    keeping the SYCL 1.2.1 header path <CL/sycl.hpp> for the time being.

--- tests/common/sycl.h
+++ tests/common/sycl.h
@@ -21,4 +21,8 @@
 #include <CL/sycl.hpp>
 #endif
 
+// TODO: Switch to canonical <sycl/sycl.hpp> header
+// once CTS has been fully converted to ::sycl namespace
+namespace sycl = ::cl::sycl;
+
 #endif  // __SYCLCTS_TESTS_COMMON_H
