23a9027dc3e7bc94619efde532e28a94d8c74d37
Yuriy Kochetkov <yuriyx.kochetkov@intel.com>
2021-04-14 14:16:04 +0300

    Add usage example for run_variadic

--- util/variadic.h
+++ util/variadic.h
@@ -15,6 +15,35 @@
 /**
  * @brief Can be used to run functors with parameter pack and custom parameters
  * @tparam functorT Functor to use
+ * @details The intent is to use a subset of collection to generate a given
+ *          number of the functor parameters.
+ *          Let's say we have:
+ *          - events: an array of SYCL device_events
+ *          - run_wait_for: some functor which can wait on any number of
+ *            events provided as template parameter pack
+ *
+ *          If we wanted to call this functor for a single event, we could use
+ *
+ *              run_wait_for{}(events[0])
+ *
+ *          If we wanted to call this functor for 32 events, we could use
+ *
+ *              run_wait_for{}(events[0], events[1], ... events[31])
+ *
+ *          Using this class we can do it in a uniform way for any constexpr
+ *          number of events:
+ *
+ *              run_variadic<run_wait_for>::with<1>(events)
+ *              run_variadic<run_wait_for>::with<32>(events)
+ *
+ *          It also works with template instantiations and additional parameters
+ *          for the functor call; so instead of calling
+ *
+ *              run_wait_for<args>{}(a, b, events[0], events[1], ... events[31])
+ *
+ *          we can use
+ *
+ *              run_variadic<run_wait_for<args>>::with<32>(events, a, b)
  */
 template <class functorT>
 struct run_variadic {
