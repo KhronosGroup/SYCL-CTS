ae580da8cbcbccd8124061618afadbaf641c8c6d
Kochetkov, Yuriy <yuriyx.kochetkov@intel.com>
2021-04-30 11:53:11 +0300

    Provide result verification for synchronous exceptions
    Co-authored-by: Zhang, JieZ <jiez.zhang@intel.com>

--- tests/exception_handling/synchronous_exceptions.cpp
+++ tests/exception_handling/synchronous_exceptions.cpp
@@ -8,10 +8,89 @@
 
 #include "../common/common.h"
 
+#include <limits>
+
 #define TEST_NAME synchronous_exceptions
 
 namespace TEST_NAMESPACE {
 using namespace sycl_cts;
+
+class custom_exception : public sycl::exception {
+ public:
+  struct reference {
+    static std::string what() {
+      return "custom_exception";
+    }
+    static std::error_code code() {
+      return sycl::make_error_code(sycl::errc::runtime);
+    }
+    static const std::error_category& category() {
+      return sycl::sycl_category();
+    }
+  };
+  custom_exception(sycl::context ctx) :
+      sycl::exception(ctx, reference::code(), reference::what()) {}
+};
+
+/** @brief Verify exception propagation from command group
+ */
+void check_exception_api(util::logger &log) {
+  auto q = util::get_cts_object::queue();
+  auto ctx = q.get_context();
+
+  try {
+    q.submit([&](cl::sycl::handler &cgh) {
+      cgh.single_task<class TEST_NAME>([=]() {});
+
+      throw custom_exception(ctx);
+    });
+    q.wait_and_throw();
+  } catch (const custom_exception &e) {
+    log_exception(log, e);
+
+    if (e.what() != custom_exception::reference::what()) {
+      FAIL(log, "invalid value for what()");
+    }
+    if (e.code() != custom_exception::reference::code()) {
+      FAIL(log, "invalid value for code()");
+    }
+    if (e.category() != custom_exception::reference::category()) {
+      FAIL(log, "invalid value for category()");
+    }
+    if (!e.has_context()) {
+      FAIL(log, "invalid value for has_context()");
+    } else if (e.get_context() != ctx) {
+      FAIL(log, "invalid value for get_context()");
+    }
+    return;
+  }
+  FAIL(log, "No expected exception was thrown");
+}
+
+/** @brief Verify exception usage for synchronous errors
+ */
+void check_exception_usage(util::logger &log) {
+  try {
+    auto device = util::get_cts_object::device();
+    const auto partition = info::partition_property::partition_equally;
+    const size_t count = std::numeric_limits<size_t>::max();
+
+    // Either errc::feature_not_supported or errc::invalid should be thrown
+    device.create_sub_devices<partition>(count);
+
+  } catch (const sycl::exception &e) {
+    const auto code = e.code();
+
+    if ((code == sycl::errc::feature_not_supported) ||
+        (code == sycl::errc::invalid)) {
+      log_exception(log, e);
+      return;
+    }
+    // Unexpected exception; re-trow
+    throw;
+  }
+  FAIL(log, "No expected exception was thrown");
+}
 
 /**
  */
@@ -26,25 +105,14 @@ class TEST_NAME : public util::test_base {
   /** execute the test
    */
   void run(util::logger &log) override {
-    cts_selector selector;
-    cl::sycl::queue q(selector);
-
     try {
-      q.submit([&](cl::sycl::handler &cgh) {
-        cgh.single_task<class TEST_NAME>([=]() {});
-      });
+      check_exception_api(log);
+      check_exception_usage(log);
     } catch (const cl::sycl::exception &e) {
-      // Check methods
-      cl::sycl::string_class sc = e.what();
-      if (e.has_context()) {
-        cl::sycl::context c = e.get_context();
-      }
-      cl::sycl::cl_int ci = e.get_cl_code();
-
       log_exception(log, e);
-      FAIL(log, "An exception should not really have been thrown");
+      auto errorMsg = std::string("a SYCL exception was caught: ") + e.what();
+      FAIL(log, errorMsg);
     }
-    q.wait_and_throw();
   }
 };
 
