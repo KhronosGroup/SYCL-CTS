9a6354a6fd0439433f928452250c5bfeb841b0af
Vasily Trikolich <vasilyx.tricolich@intel.com>
2021-07-29 17:35:37 +0300

    Add test for specialization constants defined various ways
    Provides tests for specialization constants defined various ways

---
+++ tests/specialization_constants/specialization_constants_defined_various_ways_core.cpp
@@ -0,0 +1,39 @@
+/*******************************************************************************
+//
+//  SYCL 2020 Conformance Test Suite
+//
+//  Provides tests for specialization constants defined in various ways
+//
+*******************************************************************************/
+
+#include "../common/common.h"
+#include "../common/type_coverage.h"
+#include "specialization_constants_defined_various_ways.h"
+
+#define TEST_NAME specialization_constants_defined_various_ways_core
+
+namespace TEST_NAMESPACE {
+using namespace sycl_cts;
+
+/** test specialization constants
+ */
+class TEST_NAME : public sycl_cts::util::test_base {
+ public:
+  /** return information about this test
+   */
+  void get_info(test_base::info &out) const override {
+    set_test_info(out, TOSTRING(TEST_NAME), TEST_FILE);
+  }
+
+  /** execute the test
+   */
+  void run(util::logger &log) override {
+    using namespace specialization_constants_defined_various_ways;
+    sc_run_test_core<get_spec_const::sc_no_kernel_bundle>(log);
+  }
+};
+
+// construction of this proxy will register the above test
+util::test_proxy<TEST_NAME> proxy;
+
+}  // namespace TEST_NAMESPACE
