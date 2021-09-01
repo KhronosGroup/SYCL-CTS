ea01244dfebbd74a554c8bd692a032b6d559116f
Vasily Trikolich <vasilyx.tricolich@intel.com>
2021-09-01 13:31:15 +0300

    Apply clang-format for tests/buffer/buffer_constructors_core.cpp

--- tests/buffer/buffer_constructors_core.cpp
+++ tests/buffer/buffer_constructors_core.cpp
@@ -9,8 +9,8 @@
 #include "../common/common.h"
 #include "../common/type_coverage.h"
 #include "../common/type_list.h"
-#include "buffer_type_list.h"
 #include "buffer_constructors_common.h"
+#include "buffer_type_list.h"
 
 #define TEST_NAME buffer_constructors_core
 
@@ -20,7 +20,7 @@ using namespace sycl_cts;
  * test sycl::buffer initialization
  */
 class TEST_NAME : public sycl_cts::util::test_base {
-public:
+ public:
   /** return information about this test
    */
   void get_info(test_base::info &out) const override {
@@ -30,58 +30,57 @@ public:
   /** execute the test
    */
   void run(util::logger &log) override {
-
 #ifdef SYCL_CTS_ENABLE_FULL_CONFORMANCE
-      for_all_types_and_vectors<
-          buffer_constructors_common::check_buffer_ctors_for_type>(
-          get_cts_types::vector_types, log);
+    for_all_types_and_vectors<
+        buffer_constructors_common::check_buffer_ctors_for_type>(
+        get_cts_types::vector_types, log);
 #ifdef INT8_MAX
-      for_type_and_vectors<
-          buffer_constructors_common::check_buffer_ctors_for_type, std::int8_t>(
-          log, "std::int8_t");
+    for_type_and_vectors<
+        buffer_constructors_common::check_buffer_ctors_for_type, std::int8_t>(
+        log, "std::int8_t");
 #endif
 #ifdef UINT8_MAX
-      for_type_and_vectors<
-          buffer_constructors_common::check_buffer_ctors_for_type,
-          std::uint8_t>(log, "std::uint8_t");
+    for_type_and_vectors<
+        buffer_constructors_common::check_buffer_ctors_for_type, std::uint8_t>(
+        log, "std::uint8_t");
 #endif
 #ifdef INT16_MAX
-      for_type_and_vectors<
-          buffer_constructors_common::check_buffer_ctors_for_type,
-          std::int16_t>(log, "std::int16_t");
+    for_type_and_vectors<
+        buffer_constructors_common::check_buffer_ctors_for_type, std::int16_t>(
+        log, "std::int16_t");
 #endif
 #ifdef UINT16_MAX
-      for_type_and_vectors<
-          buffer_constructors_common::check_buffer_ctors_for_type,
-          std::uint16_t>(log, "std::uint16_t");
+    for_type_and_vectors<
+        buffer_constructors_common::check_buffer_ctors_for_type, std::uint16_t>(
+        log, "std::uint16_t");
 #endif
 #ifdef INT32_MAX
-      for_type_and_vectors<
-          buffer_constructors_common::check_buffer_ctors_for_type,
-          std::int32_t>(log, "std::int32_t");
+    for_type_and_vectors<
+        buffer_constructors_common::check_buffer_ctors_for_type, std::int32_t>(
+        log, "std::int32_t");
 #endif
 #ifdef UINT32_MAX
-      for_type_and_vectors<
-          buffer_constructors_common::check_buffer_ctors_for_type,
-          std::uint32_t>(log, "std::uint32_t");
+    for_type_and_vectors<
+        buffer_constructors_common::check_buffer_ctors_for_type, std::uint32_t>(
+        log, "std::uint32_t");
 #endif
 #ifdef INT64_MAX
-      for_type_and_vectors<
-          buffer_constructors_common::check_buffer_ctors_for_type,
-          std::int64_t>(log, "std::int64_t");
+    for_type_and_vectors<
+        buffer_constructors_common::check_buffer_ctors_for_type, std::int64_t>(
+        log, "std::int64_t");
 #endif
 #ifdef UINT64_MAX
-      for_type_and_vectors<
-          buffer_constructors_common::check_buffer_ctors_for_type,
-          std::uint64_t>(log, "std::uint64_t");
+    for_type_and_vectors<
+        buffer_constructors_common::check_buffer_ctors_for_type, std::uint64_t>(
+        log, "std::uint64_t");
 #endif
 #else
-      for_all_types_and_vectors<
-          buffer_constructors_common::check_buffer_ctors_for_type>(
-          get_buffer_types::vector_types, log);
-#endif // SYCL_CTS_ENABLE_FULL_CONFORMANCE
-      for_all_types<buffer_constructors_common::check_buffer_ctors_for_type>(
-          get_buffer_types::scalar_types, log);
+    for_all_types_and_vectors<
+        buffer_constructors_common::check_buffer_ctors_for_type>(
+        get_buffer_types::vector_types, log);
+#endif  // SYCL_CTS_ENABLE_FULL_CONFORMANCE
+    for_all_types<buffer_constructors_common::check_buffer_ctors_for_type>(
+        get_buffer_types::scalar_types, log);
   }
 };
 
