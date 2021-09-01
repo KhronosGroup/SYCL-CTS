b1146beac03dd1e17cbe3823e59b697e4e1f4c82
Vasily Trikolich <vasilyx.tricolich@intel.com>
2021-09-01 13:32:44 +0300

    Apply clang-format for tests/hierarchical/hierarchical_private_memory.cpp

--- tests/hierarchical/hierarchical_private_memory.cpp
+++ tests/hierarchical/hierarchical_private_memory.cpp
@@ -12,62 +12,60 @@
 
 namespace TEST_NAMESPACE {
 
-template <int dim> class kernel;
+template <int dim>
+class kernel;
 
 using namespace sycl_cts;
 
-template <int dim> void check_dim(util::logger &log) {
-    constexpr size_t globalRange1d = 6;
-    constexpr size_t globalRange2d = 2;
-    constexpr size_t globalRangeTotal = 24;
-    constexpr size_t local = globalRange2d;
-    std::vector<size_t> data(globalRangeTotal);
+template <int dim>
+void check_dim(util::logger &log) {
+  constexpr size_t globalRange1d = 6;
+  constexpr size_t globalRange2d = 2;
+  constexpr size_t globalRangeTotal = 24;
+  constexpr size_t local = globalRange2d;
+  std::vector<size_t> data(globalRangeTotal);
 
-    auto myQueue = util::get_cts_object::queue();
-    // using this scope we ensure that the buffer will update the host values
-    // after the wait_and_throw
-    {
-      sycl::buffer<size_t, 1> buf(data.data(),
-                                      sycl::range<1>(globalRangeTotal));
+  auto myQueue = util::get_cts_object::queue();
+  // using this scope we ensure that the buffer will update the host values
+  // after the wait_and_throw
+  {
+    sycl::buffer<size_t, 1> buf(data.data(), sycl::range<1>(globalRangeTotal));
 
-      myQueue.submit([&](sycl::handler &cgh) {
-        auto accessor =
-            buf.template get_access<sycl::access_mode::read_write>(cgh);
-        auto globalRange =
-            sycl_cts::util::get_cts_object::range<dim>::template get_fixed_size<
-                globalRangeTotal>(globalRange1d, globalRange2d);
-        auto localRange = sycl_cts::util::get_cts_object::range<dim>::get(
-            local, local, local);
-        auto groupRange = globalRange / localRange;
-        cgh.parallel_for_work_group<kernel<dim>>(
-            groupRange, localRange, [=](sycl::group<dim> group_pid) {
-              sycl::private_memory<size_t, dim> priv(group_pid);
+    myQueue.submit([&](sycl::handler &cgh) {
+      auto accessor =
+          buf.template get_access<sycl::access_mode::read_write>(cgh);
+      auto globalRange =
+          sycl_cts::util::get_cts_object::range<dim>::template get_fixed_size<
+              globalRangeTotal>(globalRange1d, globalRange2d);
+      auto localRange =
+          sycl_cts::util::get_cts_object::range<dim>::get(local, local, local);
+      auto groupRange = globalRange / localRange;
+      cgh.parallel_for_work_group<kernel<dim>>(
+          groupRange, localRange, [=](sycl::group<dim> group_pid) {
+            sycl::private_memory<size_t, dim> priv(group_pid);
 
-              // Assign global linear id through private memory
-              // as parallel_for_work_item scope is allocated in private memory
-              group_pid.parallel_for_work_item(
-                  [&](sycl::h_item<dim> itemID) {
-                    priv(itemID) = itemID.get_global().get_linear_id();
-                  });
-
-              group_pid.parallel_for_work_item([&](
-                  sycl::h_item<dim> itemID) {
-                accessor[itemID.get_global().get_linear_id()] = priv(itemID);
-              });
+            // Assign global linear id through private memory
+            // as parallel_for_work_item scope is allocated in private memory
+            group_pid.parallel_for_work_item([&](sycl::h_item<dim> itemID) {
+              priv(itemID) = itemID.get_global().get_linear_id();
             });
-      });
-    }
 
-    for (size_t i = 0; i < data.size(); i++) {
-      if (data[i] != i) {
-        std::string errorMessage =
-            std::string("Value for global id ") + std::to_string(i) +
-            std::string(" was not correct (") +
-            std::to_string(data[i]) + std::string(" instead of ") +
-            std::to_string(i) + ")";
-        FAIL(log, errorMessage);
-      }
+            group_pid.parallel_for_work_item([&](sycl::h_item<dim> itemID) {
+              accessor[itemID.get_global().get_linear_id()] = priv(itemID);
+            });
+          });
+    });
+  }
+
+  for (size_t i = 0; i < data.size(); i++) {
+    if (data[i] != i) {
+      std::string errorMessage =
+          std::string("Value for global id ") + std::to_string(i) +
+          std::string(" was not correct (") + std::to_string(data[i]) +
+          std::string(" instead of ") + std::to_string(i) + ")";
+      FAIL(log, errorMessage);
     }
+  }
 }
 /** test sycl::range::get(int index) return size_t
  */
