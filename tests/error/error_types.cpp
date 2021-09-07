d05d9b57610f208e513d57c063008f325b056d53
NataliaX Kopylova <nataliax.kopylova@intel.com>
2021-09-07 19:24:54 +0700

    Returned check for sycl::exception_list

---
+++ tests/error/error_types.cpp
@@ -0,0 +1,40 @@
+/*******************************************************************************
+//
+//  SYCL 2020 Conformance Test Suite
+//
+//  Copyright:	(c) 2017 by Codeplay Software LTD. All Rights Reserved.
+//
+*******************************************************************************/
+
+#include "../common/common.h"
+
+#define TEST_NAME error_types
+
+namespace error_types {
+using namespace sycl_cts;
+
+/**
+ */
+class TEST_NAME : public util::test_base {
+ public:
+  /** return information about this test
+   */
+  void get_info(test_base::info &out) const override {
+    set_test_info(out, TOSTRING(TEST_NAME), TEST_FILE);
+  }
+
+  /** execute this test
+   */
+  void run(util::logger &log) override {
+
+    /* Check that exception_list exists */
+    if (!std::is_class<sycl::exception_list>::value) {
+      FAIL(log, "Exception list is not defined as a class");
+    }
+  }
+};
+
+// construction of this proxy will register the above test
+util::test_proxy<TEST_NAME> proxy;
+
+} /* namespace header_test_2__ */
