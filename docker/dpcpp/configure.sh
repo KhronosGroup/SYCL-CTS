28df0072b04b176fd3a4052ca1ff9fc1c4740cb6
Philip Salzmann <philip.salzmann@uibk.ac.at>
2021-09-07 17:26:15 +0200

    Initial implementation of Docker-based CI
    This introduces two workflows for running CI on the SYCL CTS:
    
    1) build_ci_containers.yml builds Docker container images for all
       supported SYCL implementations and pushes them to DockerHub.
       This workflow runs periodically (every day at 05:00 UTC) but only
       performs actual work when the desired version for a given SYCL
       implementation is changed within the workflow file.
    2) cts_ci.yml runs within the per-SYCL implementation containers.
       It attempts to configure CMake and build the CTS support libraries
       (`oclmath` and `CTS::util`).

---
+++ docker/dpcpp/configure.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -o errexit -o pipefail -o noclobber -o nounset
+
+cmake . -G Ninja -B build \
+    -DSYCL_IMPLEMENTATION=DPCPP \
+    -DDPCPP_INSTALL_DIR=/sycl \
+    -DCMAKE_CXX_COMPILER=/sycl/bin/clang++ \
+    -DCMAKE_BUILD_TYPE=Release \
+    -DSYCL_CTS_ENABLE_FULL_CONFORMANCE=0
