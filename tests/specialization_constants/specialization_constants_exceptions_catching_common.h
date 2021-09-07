ce8ec733b01eb9a186189756555731566ed5667a
Vasily Trikolich <vasilyx.tricolich@intel.com>
2021-09-07 15:12:55 +0300

    Add coment about what error expect and add verificationa that exceptions was throwed

--- tests/specialization_constants/specialization_constants_exceptions_catching_common.h
+++ tests/specialization_constants/specialization_constants_exceptions_catching_common.h
@@ -30,7 +30,8 @@ class check_spec_constant_except_catch_for_type {
     // case 1: Try to get specialization constant via handler that bound to a
     // kernel_bundle
     {
-      T res = T(value_helper<T>(0));
+      bool exception_was_throwed{};
+      T res = T(get_init_value_helper<T>(0));
       const int case_num = 1;
       auto queue = sycl_cts::util::get_cts_object::queue();
 
@@ -39,26 +40,33 @@ class check_spec_constant_except_catch_for_type {
         auto k_bundle =
             sycl::get_kernel_bundle<sycl::bundle_state::executable>(context);
         cgh.use_kernel_bundle(k_bundle);
+        // we are expected that exception with sycl::errc::invalid will be
+        // throwed
         try {
           res = cgh.get_specialization_constant<spec_const<T, case_num>>();
         } catch (const sycl::exception &e) {
           if (e.code() != sycl::errc::invalid) {
             const auto errorMsg =
-                "unexpected SYCL exception was caught in case 1 for " +
+                "unexpected SYCL exception was caught in case " +
+                std::to_string(case_num) + " for " +
                 type_name_string<T>::get(type_name);
             FAIL(log, errorMsg);
             throw;
+          } else {
+            exception_was_throwed = true;
           }
         }
         cgh.single_task(spec_const_except_dummy_functor<T>{});
       });
+      CHECK_VALUE_SCALAR(log, exception_was_throwed, true);
     }
 
     // case 2: Try to set specialization constant via handler that bound to a
     // kernel_bundle
     {
+      bool exception_was_throwed{};
+      T sc_val = T(get_init_value_helper<T>(0));
       const int case_num = 2;
-      T sc_val = T(value_helper<T>(0));
       auto queue = sycl_cts::util::get_cts_object::queue();
 
       queue.submit([&](sycl::handler &cgh) {
@@ -66,19 +74,26 @@ class check_spec_constant_except_catch_for_type {
         auto k_bundle =
             sycl::get_kernel_bundle<sycl::bundle_state::executable>(context);
         cgh.use_kernel_bundle(k_bundle);
+        // we are expected that exception with sycl::errc::invalid will be
+        // throwed
         try {
           cgh.set_specialization_constant<spec_const<T, case_num>>(sc_val);
         } catch (const sycl::exception &e) {
-          if (static_cast<sycl::errc>(e.code().value()) != sycl::errc::invalid) {
+          if (static_cast<sycl::errc>(e.code().value()) !=
+              sycl::errc::invalid) {
             const auto errorMsg =
-                "unexpected SYCL exception was caught in case 2 for " +
+                "unexpected SYCL exception was caught in case " +
+                std::to_string(case_num) + " for " +
                 type_name_string<T>::get(type_name);
             FAIL(log, errorMsg);
             throw;
+          } else {
+            exception_was_throwed = true;
           }
         }
         cgh.single_task(spec_const_except_dummy_functor<T>{});
       });
+      CHECK_VALUE_SCALAR(log, exception_was_throwed, true);
     }
   }
 };
