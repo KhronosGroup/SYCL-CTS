971a123f24c686fd656482117215c87ee89cc720
Vasily Trikolich <vasilyx.tricolich@intel.com>
2021-09-08 09:06:41 +0300

    Add common files

---
+++ tests/exceptions/exceptions.h
@@ -0,0 +1,36 @@
+/*******************************************************************************
+//
+//  SYCL 2020 Conformance Test Suite
+//
+//  Common code for exceptions tests
+//
+*******************************************************************************/
+
+#ifndef __SYCL_CTS_TEST_EXCEPTIONS_H
+#define __SYCL_CTS_TEST_EXCEPTIONS_H
+
+#include "../common/common.h"
+#include <vector>
+
+namespace exceptions {
+
+static const std::vector<sycl::errc> all_err_codes{
+    sycl::errc::success,
+    sycl::errc::runtime,
+    sycl::errc::kernel,
+    sycl::errc::accessor,
+    sycl::errc::nd_range,
+    sycl::errc::event,
+    sycl::errc::kernel_argument,
+    sycl::errc::build,
+    sycl::errc::invalid,
+    sycl::errc::memory_allocation,
+    sycl::errc::platform,
+    sycl::errc::profiling,
+    sycl::errc::feature_not_supported,
+    sycl::errc::kernel_not_supported,
+    sycl::errc::backend_mismatch};
+
+}  // namespace exceptions
+
+#endif  // __SYCL_CTS_TEST_EXCEPTIONS_H
