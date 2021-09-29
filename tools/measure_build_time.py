52f004a2621c842a45994a05dff2e3f06953d853
Philip Salzmann <philip.salzmann@uibk.ac.at>
2021-09-29 12:19:03 +0200

    Also measure ComputeCpp device compiler passes
    Add support for measuring build times for the ComputeCpp device compiler
    pass that is triggered by a custom CMake build step. Make measurement
    script more robust regarding its working directory.

--- tools/measure_build_time.py
+++ tools/measure_build_time.py
@@ -10,6 +10,7 @@ import os
 import subprocess
 import sys
 
+from pathlib import Path
 from timeit import default_timer as timer
 
 args = sys.argv[1:]
@@ -19,11 +20,24 @@ args = sys.argv[1:]
 obj_file = os.path.basename(args[-3])
 src_file = args[-1]
 
+# Locate build root: The compiler may not always be launched directly from
+# within the main build directory. We want to write all results into the
+# same file within the build directory, so we have to locate it first.
+# Walk parents until we find 'CMakeCache.txt'.
+build_root = Path(os.getcwd())
+for p in [build_root] + list(build_root.parents):
+    if os.path.isfile(p / 'CMakeCache.txt'):
+        build_root = p
+        break
+
+# Make source file path relative to build directory
+src_file = os.path.relpath(src_file, build_root)
+
 ts_before = timer()
 subprocess.run(' '.join(args), shell=True)
 ts_after = timer()
 dt = ts_after - ts_before
 
-with open("build_times.log", "a") as output_file:
+with open(build_root / "build_times.log", "a") as output_file:
     print(f"{dt:.1f} {obj_file} ({src_file})",
           file=output_file)
