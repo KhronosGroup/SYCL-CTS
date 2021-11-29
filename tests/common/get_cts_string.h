7804606eacd2c642ee16b91fec8b3263b549dbbb
Vasily Trikolich <vasilyx.tricolich@intel.com>
2021-11-29 11:45:28 +0300

    Add generic function that let obtain string from bool and bundle_state

---
+++ tests/common/get_cts_string.h
@@ -0,0 +1,46 @@
+/*******************************************************************************
+//
+//  SYCL 2020 Conformance Test Suite
+//
+//  Provides tools for string representation for some types
+//
+*******************************************************************************/
+
+#ifndef __SYCLCTS_TESTS_COMMON_GET_CTS_STRING_H
+#define __SYCLCTS_TESTS_COMMON_GET_CTS_STRING_H
+
+#include "common.h"
+
+namespace sycl_cts {
+namespace get_cts_string {
+
+enum class verbosity { brief = 0, detail };
+
+inline std::string for_bool(bool flag) {
+  return flag ? "true" : "false";
+}
+
+template <sycl::bundle_state State, verbosity Level = verbosity::brief>
+inline std::string for_bundle_state() {
+  std::string result;
+  if constexpr (State == sycl::bundle_state::input) {
+    result += "input";
+  } else if constexpr (State == sycl::bundle_state::object) {
+    result += "object";
+  } else if constexpr (State == sycl::bundle_state::executable) {
+    result += "executable";
+  } else {
+    static_assert(State != State, "incorrect kernel bundle state");
+  }
+
+  if constexpr (Level > verbosity::brief) {
+    result += " kernel bundle state";
+  }
+
+  return result;
+}
+
+}  // namespace get_cts_string
+}  // namespace sycl_cts
+
+#endif  // __SYCLCTS_TESTS_COMMON_GET_CTS_STRING_H
