5817d076d8a44b0999706eeb0401abdbf400fa7c
Vasily Trikolich <vasilyx.tricolich@intel.com>
2021-12-09 11:05:50 +0300

    Replace macros name "COMPAT_CPP20" with "SYCL_CTS_COMPAT_CPP20"

--- util/cpp_compat.h
+++ util/cpp_compat.h
@@ -10,9 +10,9 @@
 #include <unordered_set>
 
 #if __cplusplus >= 202002L
-#define COMPAT_CPP20 [[deprecated]]
+#define SYCL_CTS_COMPAT_CPP20 [[deprecated]]
 #else
-#define COMPAT_CPP20
+#define SYCL_CTS_COMPAT_CPP20
 #endif
 
 namespace sycl_cts::util {
@@ -38,7 +38,7 @@ inline typename Container::size_type erase_if(Container& container, Pred pred) {
 /** @brief Provides std::erase_if from std::map available from c++20
  */
 template <class Key, class T, class Compare, class Alloc, class Pred>
-COMPAT_CPP20 static typename std::map<Key, T, Compare, Alloc>::size_type
+SYCL_CTS_COMPAT_CPP20 static typename std::map<Key, T, Compare, Alloc>::size_type
 erase_if(std::map<Key, T, Compare, Alloc>& map, Pred pred) {
   return details::erase_if(map, pred);
 }
@@ -46,7 +46,7 @@ erase_if(std::map<Key, T, Compare, Alloc>& map, Pred pred) {
 /** @brief Provides std::erase_if form std::unordered_set available from c++20
  */
 template <class Key, class Hash, class KeyEqual, class Alloc, class Pred>
-COMPAT_CPP20 static
+SYCL_CTS_COMPAT_CPP20 static
     typename std::unordered_set<Key, Hash, KeyEqual, Alloc>::size_type
     erase_if(std::unordered_set<Key, Hash, KeyEqual, Alloc>& set, Pred pred) {
   return details::erase_if(set, pred);
