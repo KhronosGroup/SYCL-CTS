bd2f0453bcaf6fa58684048632bf24a2404e0415
NataliaX Kopylova <nataliax.kopylova@intel.com>
2022-01-10 18:03:20 +0700

    Remove flag that is off by default

--- docker/computecpp/configure.sh
+++ docker/computecpp/configure.sh
@@ -7,5 +7,4 @@ cmake . -G Ninja -B build \
     -DCMAKE_PREFIX_PATH=/sycl \
     -DCMAKE_BUILD_TYPE=Release \
     -DSYCL_CTS_ENABLE_FULL_CONFORMANCE=0 \
-    -DSYCL_ENABLE_EXT_ONEAPI_PROPERTY_LIST_TESTS=0 \
     $@
