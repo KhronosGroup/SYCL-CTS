18dce742be65399837be262be035c6a8f60e1730
Romanov AndreyX <andreyx.romanov@intel.com>
2022-01-12 11:07:50 +0700

    Added tests for multi_ptr members

---
+++ tests/multi_ptr/multi_ptr_members.h
@@ -0,0 +1,128 @@
+/*******************************************************************************
+//
+//  SYCL 2020 Conformance Test Suite
+//
+//  Common code for multi_ptr members tests
+//
+*******************************************************************************/
+
+#ifndef SYCL_CTS_TEST_MULTI_PTR_MEMBERS_H
+#define SYCL_CTS_TEST_MULTI_PTR_MEMBERS_H
+
+#include "../common/common.h"
+#include "../common/get_cts_string.h"
+
+namespace multi_ptr_members {
+
+/** @brief Dummy struct with overloaded call operator that will be called in
+ *         "for_all_types" function
+ *  @tparam VariableT Variable type for type coverage
+ */
+template <typename VariableT>
+struct run_test_with_chosen_data_type {
+  /** @brief Run verification's function with provided variable type and
+   *         sycl::access::address_space and sycl::access::decorated
+   *         enumerations fields
+   *  @param log sycl_cts::util::logger class object
+   *  @param type_name a string representing the currently tested type
+   */
+  void operator()(sycl_cts::util::logger &log, const std::string &type_name) {
+    verify_members<VariableT,
+                   sycl::access::address_space::global_space,
+                   sycl::access::decorated::yes>(log, type_name);
+    verify_members<VariableT,
+                   sycl::access::address_space::local_space,
+                   sycl::access::decorated::no>(log, type_name);
+    verify_members<VariableT,
+                   sycl::access::address_space::private_space,
+                   sycl::access::decorated::yes>(log, type_name);
+    verify_members<VariableT,
+                   sycl::access::address_space::generic_space,
+                   sycl::access::decorated::no>(log, type_name);
+    verify_members<VariableT,
+                   sycl::access::address_space::global_space,
+                   sycl::access::decorated::yes>(log, type_name);
+    verify_members<VariableT,
+                   sycl::access::address_space::local_space,
+                   sycl::access::decorated::no>(log, type_name);
+    verify_members<VariableT,
+                   sycl::access::address_space::private_space,
+                   sycl::access::decorated::yes>(log, type_name);
+    verify_members<VariableT,
+                   sycl::access::address_space::generic_space,
+                   sycl::access::decorated::no>(log, type_name);
+  }
+};
+
+/** @brief Verify that sycl::multi_ptr members are equal to:
+ *          1) sycl::multi_ptr::value_type is same as provided element type
+ *          2) sycl::multi_ptr::iterator_category is same as
+ *            std::random_access_iterator_tag
+ *          3) sycl::multi_ptr::difference_type is same
+ *            as std::ptrdiff_t If decorate address is access::decorated::no:
+ *            1)sycl::multi_ptr::pointer is same as std::add_pointer_t<
+ *              value_type>
+ *            2)sycl::multi_ptr::reference is same as
+ *              std::add_lvalue_reference_t<value_type>
+ *  @tparam VariableT Variable type for type coverage
+ *  @tparam AddressSpace Field of sycl::access::address_space namespace for
+ *          multi ptr
+ *  @tparam Decorated Field of sycl::access::decorated namespace for
+ *          multi ptr
+ *  @param log sycl_cts::util::logger class object
+ *  @param type_name a string representing the currently tested type
+ */
+template <typename VariableT,
+          sycl::access::address_space AddressSpace,
+          sycl::access::decorated Decorated>
+static void verify_members(sycl_cts::util::logger &log,
+                           const std::string &type_name) {
+  auto multi_ptr{
+      sycl::multi_ptr<VariableT, AddressSpace, Decorated>()};
+
+  std::string address_type_str{
+      std::string(sycl_cts::get_cts_string::for_address_space<AddressSpace>())};
+  std::string decorated_str{
+      std::string(sycl_cts::get_cts_string::for_decorated<Decorated>())};
+  std::string log_suffix{" with address type: " + address_type_str +
+                         ", with decorated type: " + decorated_str +
+                         ", with tested type: " + type_name};
+  if (!std::is_same_v<decltype(multi_ptr)::value_type, VariableT>) {
+    FAIL(log,
+         "sycl::multi_ptr::value_type doesent equal to provided value type" +
+             log_suffix);
+  }
+  if (!std::is_same_v<decltype(multi_ptr)::difference_type, std::ptrdiff_t>) {
+    FAIL(log,
+         "sycl::multi_ptr::difference_type doesent equal to std::ptrdiff_t" +
+             log_suffix);
+  }
+  if (!std::is_same_v<decltype(multi_ptr)::iterator_category,
+                      std::random_access_iterator_tag>) {
+    FAIL(log,
+         "sycl::multi_ptr::iterator_category doesent equal to "
+         "std::random_access_iterator_tag" +
+             log_suffix);
+  }
+
+  if constexpr (Decorated == sycl::access::decorated::no) {
+    if (!std::is_same_v<decltype(multi_ptr)::pointer,
+                        std::add_pointer_t<VariableT>>) {
+      FAIL(log,
+           "sycl::multi_ptr::pointer doesent equal to "
+           "std::add_pointer_t<value_type>" +
+               log_suffix);
+    }
+    if (!std::is_same_v<decltype(multi_ptr)::reference,
+                        std::add_lvalue_reference_t<VariableT>>) {
+      FAIL(log,
+           "sycl::multi_ptr::reference doesent equal to "
+           "std::add_lvalue_reference_t<value_type>" +
+               log_suffix);
+    }
+  }
+}
+
+}  // namespace multi_ptr_members
+
+#endif  // SYCL_CTS_TEST_MULTI_PTR_MEMBERS_H
---
+++ tests/multi_ptr/multi_ptr_members_fp64.cpp
@@ -0,0 +1,45 @@
+/*******************************************************************************
+//
+//  SYCL 2020 Conformance Test Suite
+//
+//  Provides tests for multi_ptr members tests with double data type.
+//
+*******************************************************************************/
+
+#include "../common/common.h"
+#include "multi_ptr_members.h"
+
+#define TEST_NAME multi_ptr_members_fp64
+
+namespace TEST_NAMESPACE {
+using namespace sycl_cts;
+using namespace multi_ptr_members;
+
+/** tests the api for explicit pointers
+ */
+class TEST_NAME : public util::test_base {
+ public:
+  /** return information about this test
+   */
+  void get_info(test_base::info &out) const override {
+    set_test_info(out, TOSTRING(TEST_NAME), TEST_FILE);
+  }
+
+  /** execute this test
+   */
+  void run(util::logger &log) override {
+    auto queue = util::get_cts_object::queue();
+    if (!queue.get_device().has(sycl::aspect::fp16)) {
+      log.skip(
+          "Device does not support double precision floating point operations");
+      return;
+    }
+
+    run_test_with_chosen_data_type<double>{}(log, "double");
+  }
+};
+
+// register this test with the test_collection
+util::test_proxy<TEST_NAME> proxy;
+
+} /* namespace TEST_NAMESPACE */
