300bf98d902b4196611927d01f379f92ae7ba627
NataliaX Kopylova <nataliax.kopylova@intel.com>
2022-01-25 19:49:37 +0700

    Replave log.skip with WARN

--- tests/extension/oneapi_property_list/property_list_is_device_copyable.cpp
+++ tests/extension/oneapi_property_list/property_list_is_device_copyable.cpp
@@ -28,9 +28,9 @@ class TEST_NAME : public util::test_base {
    */
   void run(util::logger &log) override {
 #if !defined(SYCL_EXT_ONEAPI_PROPERTY_LIST)
-    log.skip("SYCL_EXT_ONEAPI_PROPERTY_LIST is not defined, test is skipped");
+    WARN("SYCL_EXT_ONEAPI_PROPERTY_LIST is not defined, test is skipped");
 #elif !defined(SYCL_EXT_ONEAPI_DEVICE_GLOBAL)
-    log.skip("SYCL_EXT_ONEAPI_DEVICE_GLOBAL is not defined, test is skipped");
+    WARN("SYCL_EXT_ONEAPI_DEVICE_GLOBAL is not defined, test is skipped");
 #else
     {
       using namespace sycl::ext::oneapi;
