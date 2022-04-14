31bb79eae3f7fd9cb90811dcfeb5ecaa98ca60ae
Vasily Trikolich <vasilyx.tricolich@intel.com>
2022-04-14 10:31:04 +0300

    Test for SYCL-2020 accessor exceptions tests
    The test verify that:
    - Generic accessor constructor thrown exception in different cases
    - host_accessor constructor thrown exception in different cases
    - local_accessor thrown exception when attempting to interact with
      local_accessor on device side

---
+++ tests/accessor/accessor_exceptions.cpp
@@ -0,0 +1,63 @@
+/*******************************************************************************
+//
+//  SYCL 2020 Conformance Test Suite
+//
+//  Provides tests for accessor exceptions.
+//
+// This test provides verifications for generic accessor, host_accessor and
+// local_accessor.
+//
+*******************************************************************************/
+
+#include "../common/common.h"
+#include "../common/disabled_for_test_case.h"
+
+// FIXME: re-enable when sycl::accessor is implemented
+#if !defined(__HIPSYCL__) && !defined(__COMPUTECPP__) && \
+    !defined(__SYCL_COMPILER_VERSION)
+
+#include "accessor_exceptions.hpp"
+
+using namespace accessor_tests_common;
+#endif
+
+namespace accessor_exceptions_test {
+using namespace sycl_cts;
+
+DISABLED_FOR_TEST_CASE(hipSYCL, ComputeCpp, DPCPP)
+("Generic sycl::accessor constructor exceptions test.", "[accessor]")({
+  typedef std::integral_constant<accessor_type, accessor_type::generic_accessor>
+      generic_accessor;
+#ifndef SYCL_CTS_ENABLE_FULL_CONFORMANCE
+  const auto types = get_lightweight_type_pack();
+#else
+  const auto types = get_full_conformance_type_pack();
+#endif  // SYCL_CTS_ENABLE_FULL_CONFORMANCE
+  for_all_types_vectors_marray<run_tests_with_types, generic_accessor>(types);
+});
+
+DISABLED_FOR_TEST_CASE(hipSYCL, ComputeCpp, DPCPP)
+("Local sycl::accessor constructor exceptions test.", "[accessor]")({
+  typedef std::integral_constant<accessor_type, accessor_type::local_accessor>
+      local_accessor;
+#ifndef SYCL_CTS_ENABLE_FULL_CONFORMANCE
+  const auto types = get_lightweight_type_pack();
+#else
+  const auto types = get_full_conformance_type_pack();
+#endif  // SYCL_CTS_ENABLE_FULL_CONFORMANCE
+  for_all_types_vectors_marray<run_tests_with_types, local_accessor>(types);
+});
+
+DISABLED_FOR_TEST_CASE(hipSYCL, ComputeCpp, DPCPP)
+("Host sycl::accessor constructor exceptions test.", "[accessor]")({
+  typedef std::integral_constant<accessor_type, accessor_type::host_accessor>
+      host_accessor;
+#ifndef SYCL_CTS_ENABLE_FULL_CONFORMANCE
+  const auto types = get_lightweight_type_pack();
+#else
+  const auto types = get_full_conformance_type_pack();
+#endif  // SYCL_CTS_ENABLE_FULL_CONFORMANCE
+  for_all_types_vectors_marray<run_tests_with_types, host_accessor>(types);
+});
+
+}  // namespace accessor_exceptions_test
---
+++ tests/accessor/accessor_exceptions.hpp
@@ -0,0 +1,395 @@
+/*******************************************************************************
+//
+//  SYCL 2020 Conformance Test Suite
+//
+//  This file provides functions for tests on accessor exceptions.
+//
+*******************************************************************************/
+#ifndef SYCL_CTS_GENERIC_ACCESSOR_PROPERTIES_H
+#define SYCL_CTS_GENERIC_ACCESSOR_PROPERTIES_H
+
+#include "../../util/usm_helper.h"
+#include "accessor_common.h"
+#include "catch2/catch_test_macros.hpp"
+
+#include "accessor_stub.hpp"
+
+#include <type_traits>
+
+namespace accessor_exceptions_test {
+
+// Class used as a kernel ID.
+template <accessor_tests_common::accessor_type, typename, int,
+          sycl_stub::target>
+struct Kernel;
+
+/**
+ * @brief Function helps to verify that constructors accessor really thrown
+ *        exceptions.
+ * @tparam AccType Current type of the accessor: generic_accessor,
+ *         local_accessor, or host_accessor
+ * @tparam DataT Current data type
+ * @tparam Dimension Dimension size
+ * @tparam Target Current target
+ * @tparam GetAccFunctorT Type of the functor that will be use to interact with
+ *         accessor
+ */
+template <accessor_tests_common::accessor_type AccType, typename DataT,
+          int Dimension, sycl_stub::target Target, typename GetAccFunctorT>
+void check_exception(GetAccFunctorT construct_acc) {
+  auto queue = util::get_cts_object::queue();
+  DataT some_data(expected_val);
+  auto r = util::get_cts_object::range<Dimension>::get(1, 1, 1);
+  {
+    sycl::buffer<DataT, Dimension> data_buf(&some_data, r);
+
+    if constexpr (AccType == accessor_type::generic_accessor) {
+      auto action = [&] {
+        queue.submit([&](sycl::handler& cgh) { construct_acc(cgh, data_buf); })
+            .wait();
+      };
+      CHECK_THROWS_MATCHES(
+          action, sycl::exception,
+          sycl_cts::util::equals_exception(sycl::errc::invalid));
+    } else if constexpr (AccType ==
+                         accessor_tests_common::accessor_type::host_accessor) {
+      auto action = [&] {
+        queue.submit([&](sycl::handler& cgh) { construct_acc(cgh, data_buf); })
+            .wait();
+      };
+      CHECK_THROWS_MATCHES(
+          action, sycl::exception,
+          sycl_cts::util::equals_exception(sycl::errc::invalid));
+    } else if constexpr (AccType ==
+                         accessor_tests_common::accessor_type::local_accessor) {
+      auto action = [&] { construct_acc(queue); };
+
+      CHECK_THROWS_MATCHES(
+          action, sycl::exception,
+          sycl_cts::util::equals_exception(sycl::errc::kernel_argument));
+    }
+  }
+}
+
+/**
+ * @brief Provides functor that lets verify that local_accessor really thrown
+ *        exception.
+ * @tparam DataT Current data type
+ * @tparam Dimension Dimension size
+ * @tparam Target Current target
+ * @tparam AccType Current type of the accessor: generic_accessor,
+ *         local_accessor, or host_accessor
+ * @param type_name Current data type string representation
+ * @param access_mode_name Current access mode string representation
+ * @param target_name Current target string representation
+ */
+template <typename DataT, int Dimension, sycl_stub::target Target,
+          accessor_tests_common::accessor_type AccType>
+void test_exception_for_local_acc(const std::string& type_name,
+                                  const std::string& access_mode_name,
+                                  const std::string& target_name) {
+  auto section_name = get_section_name<Dimension>(
+      type_name, access_mode_name, target_name,
+      "Expecting exception when attempting to interact with local_accessor on "
+      "device side.");
+  SECTION(section_name) {
+    auto construct_acc = [](sycl::queue& queue) {
+      // Use a variable to avoid device code optimisation.
+      auto is_empty =
+          usm_helper::allocate_usm_memory<sycl::usm::alloc::shared, bool>(
+              queue);
+      queue
+          .submit([&](sycl::handler& cgh) {
+            auto is_empty_ptr = is_empty.get();
+            sycl_stub::local_accessor<DataT, Dimension> local_acc(cgh);
+            cgh.single_task<Kernel<AccType, DataT, Dimension, Target>>(
+                [=](sycl::kernel_handler cgh) {
+                  // Some interactions to avoid device code optimisation.
+                  *is_empty_ptr = local_acc.empty();
+                });
+          })
+          .wait();
+    };
+    check_exception<AccType, DataT, Dimension, Target>(construct_acc);
+  }
+}
+
+/**
+ * @brief Provides functors that lets verify that host_accessor really thrown
+ *        exception.
+ * @tparam DataT Current data type
+ * @tparam Dimension Dimension size
+ * @tparam Target Current target
+ * @tparam AccType Current type of the accessor: generic_accessor,
+ *         local_accessor, or host_accessor
+ * @param type_name Current data type string representation
+ * @param access_mode_name Current access mode string representation
+ * @param target_name Current target string representation
+ */
+template <typename DataT, int Dimension, sycl_stub::target Target,
+          accessor_tests_common::accessor_type AccType>
+void test_exception_for_host_acc(const std::string& type_name,
+                                 const std::string& access_mode_name,
+                                 const std::string& target_name) {
+  auto great_range = util::get_cts_object::range<Dimension>::get(10, 10, 10);
+  auto default_range = util::get_cts_object::range<Dimension>::get(1, 1, 1);
+  auto id = util::get_cts_object::id<Dimension>::get(1, 1, 1);
+
+  auto section_name = get_section_name<Dimension>(
+      type_name, access_mode_name, target_name,
+      "Expecting exception when attempting to construct host_accessor from "
+      "buffer and range. In case, the range exceeds the range of buffer "
+      "in any dimension.");
+  SECTION(section_name) {
+    auto construct_acc = [&great_range](
+                             sycl::handler& cgh,
+                             sycl::buffer<DataT, Dimension> data_buf) {
+      sycl_stub::host_accessor<DataT, Dimension, sycl::access_mode::read>(
+          data_buf, great_range);
+    };
+    check_exception<AccType, DataT, Dimension, Target>(construct_acc);
+  }
+
+  section_name = get_section_name<Dimension>(
+      type_name, access_mode_name, target_name,
+      "Expecting exception when attempting to construct host_accessor from "
+      "buffer, range and tag. In case, the range exceeds the range of buffer "
+      "in any dimension.");
+  SECTION(section_name) {
+    auto construct_acc = [&great_range](
+                             sycl::handler& cgh,
+                             sycl::buffer<DataT, Dimension> data_buf) {
+      sycl_stub::host_accessor<DataT, Dimension, sycl::access_mode::read>(
+          data_buf, great_range, sycl_stub::write_only);
+    };
+    check_exception<AccType, DataT, Dimension, Target>(construct_acc);
+  }
+
+  section_name = get_section_name<Dimension>(
+      type_name, access_mode_name, target_name,
+      "Expecting exception when attempting to construct host_accessor from "
+      "buffer, and range and id. In case, when the sum of range and offset "
+      "exceeds the range of buffer in any dimension.");
+  SECTION(section_name) {
+    auto construct_acc = [&default_range, id](
+                             sycl::handler& cgh,
+                             sycl::buffer<DataT, Dimension> data_buf) {
+      sycl_stub::host_accessor<DataT, Dimension, sycl::access_mode::read>(
+          data_buf, default_range, id);
+    };
+    check_exception<AccType, DataT, Dimension, Target>(construct_acc);
+  }
+
+  section_name = get_section_name<Dimension>(
+      type_name, access_mode_name, target_name,
+      "Expecting exception when attempting to construct host_accessor from "
+      "buffer, and range, id and tag. In case, when the sum of range and "
+      "offset exceeds the range of buffer in any dimension.");
+  SECTION(section_name) {
+    auto construct_acc = [&default_range, id](
+                             sycl::handler& cgh,
+                             sycl::buffer<DataT, Dimension> data_buf) {
+      sycl_stub::host_accessor<DataT, Dimension, sycl::access_mode::read>(
+          data_buf, default_range, id, sycl_stub::write_only);
+    };
+    check_exception<AccType, DataT, Dimension, Target>(construct_acc);
+  }
+}
+
+/**
+ * @brief Provides functors that lets verify that generic accessor really thrown
+ *        exception.
+ * @tparam DataT Current data type
+ * @tparam Dimension Dimension size
+ * @tparam Target Current target
+ * @tparam AccType Current type of the accessor: generic_accessor,
+ *         local_accessor, or host_accessor
+ * @param type_name Current data type string representation
+ * @param access_mode_name Current access mode string representation
+ * @param target_name Current target string representation
+ */
+template <typename DataT, int Dimension, sycl_stub::target Target,
+          accessor_tests_common::accessor_type AccType>
+void test_exception_for_generic_acc(const std::string& type_name,
+                                    const std::string& access_mode_name,
+                                    const std::string& target_name) {
+  auto great_range = util::get_cts_object::range<Dimension>::get(10, 10, 10);
+  auto default_range = util::get_cts_object::range<Dimension>::get(1, 1, 1);
+  auto id = util::get_cts_object::id<Dimension>::get(1, 1, 1);
+
+  auto section_name = get_section_name<Dimension>(
+      type_name, access_mode_name, target_name,
+      "Expecting exception when attempting to construct host_accessor from "
+      "buffer, and range that range and offset with no_init property and "
+      "access_mode::read");
+  SECTION(section_name) {
+    auto construct_acc = [&great_range](
+                             sycl::handler& cgh,
+                             sycl::buffer<DataT, Dimension> data_buf) {
+      sycl_stub::host_accessor<DataT, Dimension, sycl::access_mode::read>(
+          data_buf, great_range);
+    };
+    check_exception<AccType, DataT, Dimension, Target>(construct_acc);
+  }
+
+  section_name = get_section_name<Dimension>(
+      type_name, access_mode_name, target_name,
+      "Expecting exception when attempting to construct accessor from "
+      "buffer, tag and range. In case, the range exceeds the range of buffer "
+      "in any dimension.");
+  SECTION(section_name) {
+    auto construct_acc = [&great_range](
+                             sycl::handler& cgh,
+                             sycl::buffer<DataT, Dimension> data_buf) {
+      sycl_stub::accessor<DataT, Dimension, sycl::access_mode::read, Target>(
+          data_buf, great_range, sycl_stub::write_only);
+    };
+    check_exception<AccType, DataT, Dimension, Target>(construct_acc);
+  }
+
+  section_name = get_section_name<Dimension>(
+      type_name, access_mode_name, target_name,
+      "Expecting exception when attempting to construct accessor from buffer, "
+      "and range and id. In case, when the sum of range and offset exceeds the "
+      "range of buffer in any dimension.");
+  SECTION(section_name) {
+    auto construct_acc = [&default_range, id](
+                             sycl::handler& cgh,
+                             sycl::buffer<DataT, Dimension> data_buf) {
+      sycl_stub::accessor<DataT, Dimension, sycl::access_mode::read, Target>(
+          data_buf, default_range, id);
+    };
+    check_exception<AccType, DataT, Dimension, Target>(construct_acc);
+  }
+
+  section_name = get_section_name<Dimension>(
+      type_name, access_mode_name, target_name,
+      "Expecting exception when attempting to construct accessor from buffer, "
+      "tag, range and id. In case, when the sum of range and offset exceeds "
+      "the range of buffer in any dimension.");
+  SECTION(section_name) {
+    auto construct_acc = [&default_range, id](
+                             sycl::handler& cgh,
+                             sycl::buffer<DataT, Dimension> data_buf) {
+      sycl_stub::accessor<DataT, Dimension, sycl::access_mode::read, Target>(
+          data_buf, default_range, id, sycl_stub::write_only);
+    };
+    check_exception<AccType, DataT, Dimension, Target>(construct_acc);
+  }
+
+  section_name = get_section_name<Dimension>(
+      type_name, access_mode_name, target_name,
+      "Expecting exception when attempting to construct accessor from "
+      "buffer, handler and range. In case, the range exceeds the range of "
+      "buffer in any dimension");
+  SECTION(section_name) {
+    auto construct_acc = [&great_range](
+                             sycl::handler& cgh,
+                             sycl::buffer<DataT, Dimension> data_buf) {
+      sycl_stub::accessor<DataT, Dimension, sycl::access_mode::read, Target>(
+          data_buf, cgh, great_range);
+    };
+    check_exception<AccType, DataT, Dimension, Target>(construct_acc);
+  }
+
+  section_name = get_section_name<Dimension>(
+      type_name, access_mode_name, target_name,
+      "Expecting exception when attempting to construct accessor from "
+      "buffer, handler, range and tag. In case, the range exceeds the range of "
+      "buffer in any dimension.");
+  SECTION(section_name) {
+    auto construct_acc = [&great_range](
+                             sycl::handler& cgh,
+                             sycl::buffer<DataT, Dimension> data_buf) {
+      sycl_stub::accessor<DataT, Dimension, sycl::access_mode::read, Target>(
+          data_buf, cgh, great_range, sycl_stub::write_only);
+    };
+    check_exception<AccType, DataT, Dimension, Target>(construct_acc);
+  }
+
+  section_name = get_section_name<Dimension>(
+      type_name, access_mode_name, target_name,
+      "Expecting exception when attempting to construct accessor from "
+      "buffer, handler, range and tag. In case, when the sum of range and "
+      "offset exceeds the range of buffer in any dimension.");
+  SECTION(section_name) {
+    auto construct_acc = [&default_range, id](
+                             sycl::handler& cgh,
+                             sycl::buffer<DataT, Dimension> data_buf) {
+      sycl_stub::accessor<DataT, Dimension, sycl::access_mode::read, Target>(
+          data_buf, cgh, default_range, id);
+    };
+    check_exception<AccType, DataT, Dimension, Target>(construct_acc);
+  }
+}
+
+/**
+ * @brief Struct with functor that will be used in type coverage to call all
+ *        verification functions.
+ * @tparam AccT Current type of the accessor: generic_accessor,
+ *         local_accessor, or host_accessor
+ * @tparam T Current data type
+ * @tparam AccessModeTypeT Access mode type
+ * @tparam TargetTypeT Target type
+ * @tparam DimensionTypeT Dimension type
+ * @param type_name Current data type string representation
+ * @param access_mode_name Current access mode string representation
+ * @param target_name Current target string representation
+ */
+template <typename AccT, typename T, typename AccessModeTypeT,
+          typename TargetTypeT, typename DimensionTypeT>
+class run_tests {
+  static constexpr sycl::access_mode AccessModeT = AccessModeTypeT::value;
+  static constexpr int Dimension = DimensionTypeT::value;
+  static constexpr sycl_stub::target Target = TargetTypeT::value;
+
+ public:
+  void operator()(const std::string& type_name,
+                  const std::string& access_mode_name,
+                  const std::string& target_name) {
+    constexpr accessor_tests_common::accessor_type acc_type = AccT::value;
+    if constexpr (acc_type ==
+                  accessor_tests_common::accessor_type::generic_accessor) {
+      test_exception_for_generic_acc<T, Dimension, Target, acc_type>(
+          type_name, access_mode_name, target_name);
+    } else if constexpr (acc_type ==
+                         accessor_tests_common::accessor_type::host_accessor) {
+      test_exception_for_host_acc<T, Dimension, Target, acc_type>(
+          type_name, access_mode_name, target_name);
+    } else if constexpr (acc_type ==
+                         accessor_tests_common::accessor_type::local_accessor) {
+      test_exception_for_local_acc<T, Dimension, Target, acc_type>(
+          type_name, access_mode_name, target_name);
+    }
+  }
+};
+
+/**
+ * @brief Struct that runs test with different input parameters types
+ * @tparam AccT Current type of the accessor: generic_accessor,
+ *         local_accessor, or host_accessor
+ * @tparam T Current data type
+ * @param type_name Current data type string representation
+ * @param access_mode_name Current access mode string representation
+ * @param target_name Current target string representation
+ */
+template <typename T, typename AccT>
+class run_tests_with_types {
+ public:
+  void operator()(const std::string& type_name) {
+    // Type packs instances have to be const, otherwise for_all_combination
+    // will not compile
+    const auto access_modes = get_access_modes();
+    const auto dimensions = get_dimensions();
+    const auto targets = get_targets();
+    const auto cur_type =
+        named_type_pack<T>::generate(type_name_string<T>::get(type_name));
+
+    for_all_combinations<run_tests, AccT>(cur_type, access_modes, targets,
+                                          dimensions);
+  }
+};
+
+}  // namespace accessor_exceptions_test
+
+#endif  // SYCL_CTS_GENERIC_ACCESSOR_PROPERTIES_H
