9a1ee752c1a3cbb1f73d0fabd321819802baa3d7
Vasily Tricolich <vasiliy.trikolich@auriga.com>
2022-06-06 15:00:19 +0300

    Update test:
    - Update test for obtain multi_ptr by rvalue-reference
    - Define constant address_space for destination multi_ptr

--- tests/multi_ptr/multi_ptr_convert_assignment_ops.h
+++ tests/multi_ptr/multi_ptr_convert_assignment_ops.h
@@ -20,22 +20,22 @@ namespace multi_ptr_convert_assignment_ops {
 constexpr int expected_val = 42;
 
 template <typename T, typename SrcAddrSpaceT, typename SrcIsDecorated,
-          typename DstAddrSpaceT, typename DstIsDecorated>
+          typename DstIsDecorated>
 class run_convert_assignment_operators_tests {
   static constexpr sycl::access::address_space src_space = SrcAddrSpaceT::value;
   static constexpr sycl::access::decorated src_decorated =
       SrcIsDecorated::value;
-  static constexpr sycl::access::address_space dst_space = DstAddrSpaceT::value;
   static constexpr sycl::access::decorated dst_decorated =
       DstIsDecorated::value;
   using src_multi_ptr_t = sycl::multi_ptr<T, src_space, src_decorated>;
-  using dst_multi_ptr_t = sycl::multi_ptr<T, dst_space, dst_decorated>;
+  using dst_multi_ptr_t =
+      sycl::multi_ptr<T, sycl::access::address_space::generic_space,
+                      src_decorated>;
 
  public:
   void operator()(const std::string &type_name,
                   const std::string &src_address_space_name,
                   const std::string &src_is_decorated_name,
-                  const std::string &dst_address_space_name,
                   const std::string &dst_is_decorated_name) {
     auto queue = sycl_cts::util::get_cts_object::queue();
     T value = user_def_types::get_init_value_helper<T>(expected_val);
@@ -45,12 +45,12 @@ class run_convert_assignment_operators_tests {
             .with("T", type_name)
             .with("src address_space", src_address_space_name)
             .with("src decorated", src_is_decorated_name)
-            .with("dst address_space", dst_address_space_name)
+            .with("dst address_space", "access::address_space::generic_space")
             .with("dst decorated", dst_is_decorated_name)
             .create()) {
       bool res = false;
       {
-        sycl::buffer<bool, 1> res_buf(&res, sycl::range<1>(1));
+        sycl::buffer<bool> res_buf(&res, sycl::range<1>(1));
         sycl::buffer<T> val_buffer(&value, sycl::range<1>(1));
         queue.submit([&](sycl::handler &cgh) {
           auto res_acc =
@@ -73,16 +73,16 @@ class run_convert_assignment_operators_tests {
 
     SECTION(
         section_name(
-            "Check &operator=(multi_ptr<value_type, ASP, IsDecorated>&)")
+            "Check &operator=(multi_ptr<value_type, AS, IsDecorated>&&)")
             .with("T", type_name)
             .with("src address_space", src_address_space_name)
             .with("src decorated", src_is_decorated_name)
-            .with("dst address_space", dst_address_space_name)
+            .with("dst address_space", "access::address_space::generic_space")
             .with("dst decorated", dst_is_decorated_name)
             .create()) {
       bool res = false;
       {
-        sycl::buffer<bool, 1> res_buf(&res, sycl::range<1>(1));
+        sycl::buffer<bool> res_buf(&res, sycl::range<1>(1));
         sycl::buffer<T> val_buffer(&value, sycl::range<1>(1));
         queue.submit([&](sycl::handler &cgh) {
           auto res_acc =
@@ -93,7 +93,7 @@ class run_convert_assignment_operators_tests {
             src_multi_ptr_t mptr_in(acc_for_mptr);
             dst_multi_ptr_t mptr_out;
 
-            mptr_out = mptr_in;
+            mptr_out = std::move(mptr_in);
 
             // Check that second mptr has the same value as first mptr
             res_acc[0] = *(mptr_out.get_raw()) == acc_for_mptr[0];
@@ -102,7 +102,7 @@ class run_convert_assignment_operators_tests {
       }
       CHECK(res);
     }
-    }
+  }
 };
 
 template <typename T>
@@ -113,9 +113,9 @@ class check_multi_ptr_convert_assign_for_type {
     const auto is_decorated = multi_ptr_common::get_decorated();
 
     // Run test with address_space and decorated for source multi_ptr type and
-    // with address_space and decorated for destination  multi_ptr type
+    // with decorated for destination multi_ptr type
     for_all_combinations<run_convert_assignment_operators_tests, T>(
-        address_spaces, is_decorated, address_spaces, is_decorated, type_name);
+        address_spaces, is_decorated, is_decorated, type_name);
   }
 };
 
