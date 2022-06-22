6220796c2f4e827cd4a6f48b1c141cec6cf58998
Vasily Tricolich <vasiliy.trikolich@auriga.com>
2022-06-22 12:52:34 +0300

    Move common code into separate function to avoid code duplication

--- tests/multi_ptr/multi_ptr_explicit_conversions.h
+++ tests/multi_ptr/multi_ptr_explicit_conversions.h
@@ -16,75 +16,110 @@
 namespace multi_ptr_explicit_conversions {
 
 constexpr int expected_val = 42;
+
+/**
+ * @brief Test struct that provides functions to verify multi_ptr explicit
+ *        conversions
+ * @tparam T Current data type
+ * @tparam AddrSpaceT sycl::access::address_space enumeration's field
+ * @tparam IsDecorated sycl::access::decorated enumeration's field
+ */
 template <typename T, typename AddrSpaceT, typename IsDecorated>
 class run_explicit_convert_tests {
   static constexpr sycl::access::address_space target_space = AddrSpaceT::value;
   static constexpr sycl::access::decorated decorated = IsDecorated::value;
+  template <typename T1>
   using multi_ptr_t =
-      sycl::multi_ptr<T, sycl::access::address_space::generic_space, decorated>;
+      sycl::multi_ptr<T1, sycl::access::address_space::generic_space,
+                      decorated>;
+  // An alias that provides right multi_ptr type that depends on is the current
+  // data type const or not
+  template <typename T1>
+  using input_multi_ptr_t =
+      std::conditional_t<std::is_const_v<T1>, const multi_ptr_t<T1>,
+                         multi_ptr_t<T1>>;
+
+  /**
+   * @brief Main test function that runs test verification
+   * @tparam T1 Current data type
+   * @param queue sycl::queue where a kernel will be executed
+   * @param r sycl::range that will be used in parallel_for
+   */
+  template <typename T1>
+  void run_test(sycl::queue &queue, sycl::range &r) {
+    bool res = false;
+    T value = user_def_types::get_init_value_helper<T>(expected_val);
+    {
+      sycl::buffer<bool> res_buf(&res, sycl::range<1>(1));
+      sycl::buffer<T> val_buffer(&value, sycl::range<1>(1));
+      queue.submit([&](sycl::handler &cgh) {
+        auto res_acc =
+            res_buf.template get_access<sycl::access_mode::write>(cgh);
+        if constexpr (target_space ==
+                      sycl::access::address_space::global_space) {
+          auto val_acc =
+              val_buffer.template get_access<sycl::access_mode::read>(cgh);
+          cgh.single_task([=] {
+            input_multi_ptr_t<T1> mptr_in(val_acc);
+            auto mptr_out =
+                sycl::multi_ptr<T, target_space, decorated>(mptr_in);
+
+            // Check that second mptr has the same value as first mptr
+            res_acc[0] = *(mptr_out.get_raw()) == val_acc[0];
+          });
+        } else {
+          sycl::local_accessor<T> local_acc(r, cgh);
+          cgh.parallel_for(sycl::nd_range<1>(r, r), [=](sycl::nd_item<1> item) {
+            if constexpr (target_space ==
+                          sycl::access::address_space::local_space) {
+              auto ref = local_acc[0];
+              value_operations::assign(ref, expected_val);
+              input_multi_ptr_t<T1> mptr_in(local_acc);
+
+              auto mptr_out =
+                  sycl::multi_ptr<T, target_space, decorated>(mptr_in);
+              res_acc[0] = (*(mptr_out.get()) == ref);
+            } else {
+              T private_val =
+                  user_def_types::get_init_value_helper<T>(expected_val);
+
+              input_multi_ptr_t<T1> mptr_in = sycl::address_space_cast<
+                  sycl::access::address_space::generic_space, decorated, T>(
+                  &private_val);
+
+              auto mptr_out =
+                  sycl::multi_ptr<T, target_space, decorated>(mptr_in);
+              res_acc[0] = *(mptr_out.get_raw()) == private_val;
+            }
+          });
+        }
+      });
+    }
+    CHECK(res);
+  }
 
  public:
+  /**
+   * @brief Call operator that will call the main test functon
+   * @param type_name Current data type string representation
+   * @param target_address_space_name Current sycl::access::address_space string
+   *        representation
+   * @param is_decorated_name Current sycl::access::decorated string
+   *        representation
+   */
   void operator()(const std::string &type_name,
                   const std::string &target_address_space_name,
                   const std::string &is_decorated_name) {
     auto queue = sycl_cts::util::get_cts_object::queue();
-    T value = user_def_types::get_init_value_helper<T>(expected_val);
     auto r = sycl::range(1);
+
     SECTION(
         section_name("Check multi_ptr<T, target_address_space, IsDecorated>()")
             .with("T", type_name)
             .with("target address_space", target_address_space_name)
             .with("decorated", is_decorated_name)
             .create()) {
-      bool res = false;
-      {
-        sycl::buffer<bool> res_buf(&res, sycl::range<1>(1));
-        sycl::buffer<T> val_buffer(&value, sycl::range<1>(1));
-        queue.submit([&](sycl::handler &cgh) {
-          auto res_acc =
-              res_buf.template get_access<sycl::access_mode::write>(cgh);
-          if constexpr (target_space ==
-                        sycl::access::address_space::global_space) {
-            auto val_acc =
-                val_buffer.template get_access<sycl::access_mode::read>(cgh);
-            cgh.single_task([=] {
-              multi_ptr_t mptr_in(val_acc);
-              auto mptr_out =
-                  sycl::multi_ptr<T, target_space, decorated>(mptr_in);
-
-              // Check that second mptr has the same value as first mptr
-              res_acc[0] = *(mptr_out.get_raw()) == val_acc[0];
-            });
-          } else {
-            sycl::local_accessor<T> local_acc(r, cgh);
-            cgh.parallel_for(
-                sycl::nd_range<1>(r, r), [=](sycl::nd_item<1> item) {
-                  if constexpr (target_space ==
-                                sycl::access::address_space::local_space) {
-                    auto ref = local_acc[0];
-                    value_operations::assign(ref, expected_val);
-                    multi_ptr_t mptr_in(local_acc);
-
-                    auto mptr_out =
-                        sycl::multi_ptr<T, target_space, decorated>(mptr_in);
-                    res_acc[0] = (*(mptr_out.get()) == ref);
-                  } else {
-                    T private_val =
-                        user_def_types::get_init_value_helper<T>(expected_val);
-
-                    multi_ptr_t mptr_in = sycl::address_space_cast<
-                        sycl::access::address_space::generic_space, decorated,
-                        T>(&private_val);
-
-                    auto mptr_out =
-                        sycl::multi_ptr<T, target_space, decorated>(mptr_in);
-                    res_acc[0] = *(mptr_out.get_raw()) == private_val;
-                  }
-                });
-          }
-        });
-      }
-      CHECK(res);
+      run_test<T>(queue, r);
     }
 
     SECTION(section_name("Check multi_ptr<const T, target_address_space, "
@@ -93,55 +128,7 @@ class run_explicit_convert_tests {
                 .with("target address_space", target_address_space_name)
                 .with("decorated", is_decorated_name)
                 .create()) {
-      bool res = false;
-      {
-        sycl::buffer<bool> res_buf(&res, sycl::range<1>(1));
-        sycl::buffer<T> val_buffer(&value, sycl::range<1>(1));
-        queue.submit([&](sycl::handler &cgh) {
-          auto res_acc =
-              res_buf.template get_access<sycl::access_mode::write>(cgh);
-          if constexpr (target_space ==
-                        sycl::access::address_space::global_space) {
-            auto val_acc =
-                val_buffer.template get_access<sycl::access_mode::read>(cgh);
-            cgh.single_task([=] {
-              const multi_ptr_t mptr_in(val_acc);
-              auto mptr_out =
-                  sycl::multi_ptr<const T, target_space, decorated>(mptr_in);
-
-              // Check that second mptr has the same value as first mptr
-              res_acc[0] = *(mptr_out.get_raw()) == val_acc[0];
-            });
-          } else {
-            sycl::local_accessor<T> local_acc(r, cgh);
-            cgh.parallel_for(sycl::nd_range<1>(r, r), [=](sycl::nd_item<1>
-                                                              item) {
-              if constexpr (target_space ==
-                            sycl::access::address_space::local_space) {
-                auto ref = local_acc[0];
-                value_operations::assign(ref, expected_val);
-                const multi_ptr_t mptr_in(local_acc);
-
-                auto mptr_out =
-                    sycl::multi_ptr<const T, target_space, decorated>(mptr_in);
-                res_acc[0] = (*(mptr_out.get()) == ref);
-              } else {
-                T private_val =
-                    user_def_types::get_init_value_helper<T>(expected_val);
-
-                const multi_ptr_t mptr_in = sycl::address_space_cast<
-                    sycl::access::address_space::generic_space, decorated, T>(
-                    &private_val);
-
-                auto mptr_out =
-                    sycl::multi_ptr<const T, target_space, decorated>(mptr_in);
-                res_acc[0] = *(mptr_out.get_raw()) == private_val;
-              }
-            });
-          }
-        });
-      }
-      CHECK(res);
+      run_test<const T>(queue, r);
     }
   }
 };
