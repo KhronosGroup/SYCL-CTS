859025afa1e934e4d09972b30dc87d8c2c9e856d
Vasily Tricolich <vasiliy.trikolich@auriga.com>
2022-07-19 12:31:35 +0300

    Add test on access to the memory shared among work-items
    These tests provides verification that local_accessor can
    access to the memory shared among work-items

---
+++ tests/accessor/local_accessor_access_among_work_items.h
@@ -0,0 +1,91 @@
+/*******************************************************************************
+//
+//  SYCL 2020 Conformance Test Suite
+//
+//  Provides tests for the properties of sycl::local_accessor that can access to
+//  the memory shared among work-items
+//
+*******************************************************************************/
+#ifndef SYCL_CTS_LOCAL_ACCESSOR_ACCESS_AMONG_WORK_ITEMS_H
+#define SYCL_CTS_LOCAL_ACCESSOR_ACCESS_AMONG_WORK_ITEMS_H
+#include "accessor_common.h"
+
+namespace local_accessor_access_among_work_items {
+using namespace sycl_cts;
+using namespace accessor_tests_common;
+
+/**
+ * @brief Provides a functor that provides verification that local_accessor can
+ *        access to the memory shared among work-items
+ * @tparam T Current data type
+ * @tparam DimensionTypeT Current current dimension size
+ */
+template <typename T, typename DimensionTypeT>
+class run_test {
+  static constexpr int Dimension = DimensionT::value;
+
+ public:
+  /**
+   * @brief Functor that provides verification that local_accessor can access to
+   *        the memory shared among work-items
+   * @param type_name Current data type string representation
+   */
+  void operator()(const std::string& type_name) {
+    auto queue = util::get_cts_object::queue();
+
+    auto section_name = get_section_name<Dimension>(
+        type_name,
+        "Verify possibility to access to the memory shared among work-items. "
+        "[local_accessor]");
+    SECTION(section_name) {
+      T values_arr[2] = {1, 2};
+      const sycl::range range(2);
+      constexpr T new_value = 5;
+      constexpr T new_assign_val = 20;
+      bool acc_val_is_equal_to_expected = false;
+      {
+        sycl::buffer<bool> val_is_equal_to_expected_buffer(
+            &acc_val_is_equal_to_expected, sycl::range(1));
+        queue.submit([&](sycl::handler& cgh) {
+          auto val_is_equal_to_expected_acc =
+              val_is_equal_to_expected_buffer
+                  .template get_access<sycl::access_mode::write>(cgh);
+          cgh.parallel_for(
+              sycl::nd_range(range, range), [=](sycl::nd_item item) {
+                sycl::local_accessor<T, Dimension> acc(range, cgh);
+                T& acc_elem = array_acc[0];
+
+                if (item.get_global_id(0) != 0) {
+                  acc_elem = new_value;
+                }
+
+                // Wait until the code above will be completed in all work-items
+                sycl::group_barrier(item.get_group());
+
+                if (item.get_global_id(0) == 0 && acc_elem == new_value) {
+                  acc_elem = new_assign_val;
+                }
+
+                val_is_equal_to_expected_acc[0] = acc_elem == new_assign_val;
+              });
+        });
+      }
+      CHECK(acc_val_is_equal_to_expected);
+    }
+  }
+};
+
+template <typename T>
+class run_local_accessor_access_among_work_items_tests {
+ public:
+  void operator()(const std::string& type_name) {
+    // Type packs instances have to be const, otherwise for_all_combination will
+    // not compile
+    const auto dimensions = get_dimensions();
+
+    for_all_combinations<run_test, T>(dimensions, type_name);
+  }
+};
+}  // namespace local_accessor_access_among_work_items
+
+#endif  // SYCL_CTS_LOCAL_ACCESSOR_ACCESS_AMONG_WORK_ITEMS_H
---
+++ tests/accessor/local_accessor_access_among_work_items_core.cpp
@@ -0,0 +1,34 @@
+/*******************************************************************************
+//
+//  SYCL 2020 Conformance Test Suite
+//
+//  Provides tests for local_accessor.
+//
+//  This test provides verifications that local_accessor can access to the
+//  memory shared among work-items.
+//
+*******************************************************************************/
+
+#include "../common/common.h"
+#include "../common/disabled_for_test_case.h"
+
+// FIXME: re-enable when sycl::accessor is implemented
+#if !SYCL_CTS_COMPILING_WITH_HIPSYCL && !SYCL_CTS_COMPILING_WITH_COMPUTECPP && \
+    !SYCL_CTS_COMPILING_WITH_DPCPP
+#include "local_accessor_access_among_work_items.h"
+
+using namespace local_accessor_access_among_work_items;
+using namespace accessor_tests_common;
+#endif
+
+namespace local_accessor_access_among_work_items_core {
+
+DISABLED_FOR_TEST_CASE(hipSYCL, ComputeCpp, DPCPP)
+("sycl::host_accessor properties. core types", "[accessor]")({
+  const auto types = get_conformance_type_pack();
+
+  for_all_types_vectors_marray<
+      run_local_accessor_access_among_work_items_tests>(types);
+});
+
+}  // namespace local_accessor_access_among_work_items_core
