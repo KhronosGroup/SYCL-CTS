341e8023eb171f091b08a50743674bbc9fbd16d5
Alexey Bader <alexey.bader@intel.com>
2022-09-05 07:05:02 -0700

    Fix typo.

--- tests/event/event.cpp
+++ tests/event/event.cpp
@@ -445,7 +445,7 @@ static void check_get_profiling_info_return_type() {
 }
 
 // FIXME: reenable when struct information descriptors are implemented
-DISABLED_FOR_TEST_CASE(ComputeCPP)
+DISABLED_FOR_TEST_CASE(ComputeCpp)
 ("event::get_profiling_info works as expected", "[event]")({
   // Check that queries return the expected type.
   check_get_profiling_info_return_type<
