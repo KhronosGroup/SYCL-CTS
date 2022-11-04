4cbb63a0cc70e4e402a3d8f332c8658e4e3bc6be
Larsen, Steffen <steffen.larsen@intel.com>
2022-11-04 06:27:26 -0700

    Switch usm_atomic_access to Catch2
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- tests/usm/usm_atomic_access.h
+++ tests/usm/usm_atomic_access.h
@@ -64,18 +64,16 @@ struct kernel;
  *  @tparam AllocMemT USM allocation type
  *  @tparam CounterT Variable type from type coverage
  *  @param queue sycl::queue class object
- *  @param log sycl_cts::util::logger class object
  *  @param typeName a string representing the currently tested type
  */
 template <sycl::usm::alloc AllocMemT, typename CounterT>
-void check_atomic_access(sycl::queue &queue, sycl_cts::util::logger &log,
-                         const std::string &typeName) {
+void check_atomic_access(sycl::queue &queue, const std::string &typeName) {
   static_assert(AllocMemT != sycl::usm::alloc::device,
                 "This test can't be launched on USM device memory");
   if (!queue.get_device().has(get_atomic_memory<AllocMemT>())) {
-    log.note("Device does not support atomic access to the unified " +
-             std::string(usm_helper::get_allocation_description<AllocMemT>()) +
-             " memory allocation");
+    WARN("Device does not support atomic access to the unified " +
+         std::string(usm_helper::get_allocation_description<AllocMemT>()) +
+         " memory allocation");
     return;
   }
   auto orders =
@@ -83,7 +81,7 @@ void check_atomic_access(sycl::queue &queue, sycl_cts::util::logger &log,
           .get_info<sycl::info::device::atomic_memory_order_capabilities>();
   if (std::find(orders.begin(), orders.end(), sycl::memory_order::seq_cst) ==
       orders.end()) {
-    log.note(
+    WARN(
         "Device does not support atomics with sequentially consistent memory "
         "order");
     return;
@@ -146,26 +144,25 @@ void check_atomic_access(sycl::queue &queue, sycl_cts::util::logger &log,
     } while (std::chrono::high_resolution_clock::now() < unblocking_time);
   }
   queue.wait();
-  if (!CHECK_VALUE_SCALAR(log, *counter_ptr,
-                          2 * (number_of_repetitions * increment_value))) {
-    log.note("Test for the USM " +
-             std::string(usm_helper::get_allocation_description<AllocMemT>()) +
-             " memory allocation failed for \"" + typeName +
-             "\" underlying type");
+  bool final_counter_check =
+      *counter_ptr == 2 * (number_of_repetitions * increment_value);
+  CHECK(final_counter_check);
+  if (!final_counter_check) {
+    WARN("Test for the USM " +
+         std::string(usm_helper::get_allocation_description<AllocMemT>()) +
+         " memory allocation failed for \"" + typeName + "\" underlying type");
   }
 }
 
 /** @brief Run test with two allocation types
  *  @tparam CounterT Variable type from type coverage
  *  @param queue sycl::queue class object
- *  @param log sycl_cts::util::logger class object
  */
 template <typename CounterT>
 void run_test_with_chosen_mem_type(sycl::queue &queue,
-                                   sycl_cts::util::logger &log,
                                    const std::string &typeName) {
-  check_atomic_access<sycl::usm::alloc::host, CounterT>(queue, log, typeName);
-  check_atomic_access<sycl::usm::alloc::shared, CounterT>(queue, log, typeName);
+  check_atomic_access<sycl::usm::alloc::host, CounterT>(queue, typeName);
+  check_atomic_access<sycl::usm::alloc::shared, CounterT>(queue, typeName);
 }
 
 /** @brief Dummy functor that use in type coverage
@@ -174,14 +171,14 @@ void run_test_with_chosen_mem_type(sycl::queue &queue,
  */
 template <typename CounterT>
 struct run_all_tests {
-  void operator()(sycl::queue &queue, sycl_cts::util::logger &log, bool use_atomic_flag,
+  void operator()(sycl::queue &queue, bool use_atomic_flag,
                   const std::string &typeName) {
     if constexpr ((sizeof(CounterT) * CHAR_BIT) == 64) {
       if (!use_atomic_flag) {
         return;
       }
       if (!queue.get_device().has(sycl::aspect::atomic64)) {
-        log.note("Device does not perform 64-bit atomic operations");
+        WARN("Device does not perform 64-bit atomic operations");
         return;
       }
     } else {
@@ -189,7 +186,7 @@ struct run_all_tests {
         return;
       }
     }
-    run_test_with_chosen_mem_type<CounterT>(queue, log, typeName);
+    run_test_with_chosen_mem_type<CounterT>(queue, typeName);
   }
 };
 
--- tests/usm/usm_atomic_access_atomic64_fp64.cpp
+++ tests/usm/usm_atomic_access_atomic64_fp64.cpp
@@ -9,39 +9,22 @@
 
 #include "usm_atomic_access.h"
 
-#define TEST_NAME usm_atomic_access_atomic64_fp64
+#include "catch2/catch_test_macros.hpp"
 
-namespace TEST_NAMESPACE {
+namespace usm_atomic_access_atomic64_fp64 {
 using namespace sycl_cts;
 
-/** Test instance
- */
-class TEST_NAME : public sycl_cts::util::test_base {
- public:
-  /** return information about this test
-   */
-  void get_info(test_base::info &out) const override {
-    set_test_info(out, TOSTRING(TEST_NAME), TEST_FILE);
+TEST_CASE("Tests for usm atomics with double-precision floating points",
+          "[usm][atomic][atomic64][fp64]") {
+  auto queue{util::get_cts_object::queue()};
+  if (!queue.get_device().has(sycl::aspect::fp64)) {
+    WARN(
+        "Device does not support double precision floating point "
+        "operations. Skipping the test case.");
+    return;
   }
-
-  /** execute the test
-   */
-  void run(util::logger &log) override {
-    {
-      auto queue{util::get_cts_object::queue()};
-      if (!queue.get_device().has(sycl::aspect::fp64)) {
-        WARN(
-            "Device does not support double precision floating point "
-            "operations. Skipping the test case.");
-        return;
-      }
-      for_all_types<usm_atomic_access::run_all_tests>(
-          usm_atomic_access::get_fp64_type(), queue, log,
-          usm_atomic_access::with_atomic64);
-    }
-  }
-};
-
-// construction of this proxy will register the above test
-util::test_proxy<TEST_NAME> proxy;
-}  // namespace TEST_NAMESPACE
+  for_all_types<usm_atomic_access::run_all_tests>(
+      usm_atomic_access::get_fp64_type(), queue,
+      usm_atomic_access::with_atomic64);
+}
+}  // namespace usm_atomic_access_atomic64_fp64
