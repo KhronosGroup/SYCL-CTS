1345f47b90a6871f8972547c90c0593c4b4352cb
Nol Moonen <nol@streamhpc.com>
2023-01-06 17:18:44 +0100

    item: deprecate tests for deprecated features, add size_t conversion test

--- tests/item/item_2d.cpp
+++ tests/item/item_2d.cpp
@@ -3,7 +3,7 @@
 //  SYCL 2020 Conformance Test Suite
 //
 //  Copyright (c) 2017-2022 Codeplay Software LTD. All Rights Reserved.
-//  Copyright (c) 2022 The Khronos Group Inc.
+//  Copyright (c) 2022-2023 The Khronos Group Inc.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -21,6 +21,9 @@
 
 #include "../common/common.h"
 
+#include <functional>
+#include <numeric>
+
 #define TEST_NAME item_2d
 
 namespace test_item_2d__ {
@@ -28,54 +31,57 @@ using namespace sycl_cts;
 
 class kernel_item_2d {
  protected:
-  typedef sycl::accessor<int, 2, sycl::access_mode::read,
-                             sycl::target::device>
+  typedef sycl::accessor<int, 2, sycl::access_mode::read, sycl::target::device>
       t_readAccess;
-  typedef sycl::accessor<int, 2, sycl::access_mode::write,
-                             sycl::target::device>
+  typedef sycl::accessor<int, 2, sycl::access_mode::write, sycl::target::device>
       t_writeAccess;
 
-  t_readAccess m_x;
-  t_writeAccess m_o;
+  t_readAccess in;
+  t_writeAccess out;
+  t_writeAccess out_deprecated;
   sycl::range<2> r_exp;
   sycl::id<2> offset_exp;
 
  public:
-  kernel_item_2d(t_readAccess in_, t_writeAccess out_, sycl::range<2> r)
-      : m_x(in_), m_o(out_), r_exp(r), offset_exp(sycl::id<2>(0, 0)) {}
+  kernel_item_2d(t_readAccess in_, t_writeAccess out_,
+                 t_writeAccess out_deprecated_, sycl::range<2> r)
+      : in(in_),
+        out(out_),
+        out_deprecated(out_deprecated_),
+        r_exp(r),
+        offset_exp(sycl::id<2>(0, 0)) {}
 
   void operator()(sycl::item<2> item) const {
-    bool result = true;
-
+    bool all_correct = true;
     sycl::id<2> gid = item.get_id();
 
     size_t dim_a = item.get_id(0) + item.get_id(1);
     size_t dim_b = item[0] + item[1];
-    result &= (gid.get(0) + gid.get(1)) == dim_a &&
-              (gid.get(0) + gid.get(1)) == dim_b;
+    all_correct &= (gid.get(0) + gid.get(1)) == dim_a &&
+                   (gid.get(0) + gid.get(1)) == dim_b;
 
     sycl::range<2> localRange = item.get_range();
-    result &= localRange == r_exp;
+    all_correct &= localRange == r_exp;
 
-    // TODO: mark this check as testing deprecated functionality
+#ifdef SYCL_CTS_ENABLE_DEPRECATED_FEATURES_TESTS
     sycl::id<2> offset = item.get_offset();
-    result &= offset == offset_exp;
+    out_deprecated[item] = offset == offset_exp;
+#endif
 
     /* get work item range */
     const size_t nWidth = item.get_range(0);
     const size_t nHeight = item.get_range(1);
-    result &= nWidth == r_exp.get(0) && nHeight == r_exp.get(1);
+    all_correct &= nWidth == r_exp.get(0) && nHeight == r_exp.get(1);
 
     /* find the row major array id for this work item */
     size_t index = gid.get(1) +            /* y */
                    (gid.get(0) * nHeight); /* x */
 
-    /* get the global linear id */
+    /* get the global linear id and compare against precomputed index */
     const size_t glid = item.get_linear_id();
+    all_correct &= in[item] == static_cast<int>(index);
 
-    /* compare against the precomputed index */
-    result &= m_x[item] == static_cast<int>(index);
-    m_o[item] = result;
+    out[item] = all_correct;
   }
 };
 
@@ -88,60 +94,53 @@ void buffer_fill(int *buf, const int nWidth, const int nHeight) {
   }
 }
 
-int buffer_verify(int *buf, const int nWidth, const int nHeight) {
-  int nErrors = 0;
-  for (int i = 0; i < nWidth * nHeight; i++) {
-    nErrors += (buf[i] == 0);
-  }
-  return nErrors;
-}
-
-bool test_item_2d(util::logger &log) {
+void test_item_2d(util::logger &log) {
   const int nWidth = 8;
   const int nHeight = 8;
 
   const int nSize = nWidth * nHeight;
 
-  /* allocate host buffers */
-  std::unique_ptr<int> dataIn(new int[nSize]);
-  std::unique_ptr<int> dataOut(new int[nSize]);
+  /* allocate and clear host buffers */
+  std::vector<int> dataIn(nSize, 0);
+  std::vector<int> dataOut(nSize, 0);
+  std::vector<int> dataOutDeprecated(nSize, 0);
 
-  /* clear host buffers */
-  ::memset(dataIn.get(), 0, nSize * sizeof(int));
-  ::memset(dataOut.get(), 0, nSize * sizeof(int));
-
-  /*  */
-  buffer_fill(dataIn.get(), nWidth, nHeight);
+  buffer_fill(dataIn.data(), nWidth, nHeight);
 
   {
-    sycl::range<2> dataRange_i(nWidth, nHeight);
-    sycl::range<2> dataRange_o(nWidth, nHeight);
+    sycl::range<2> dataRange(nWidth, nHeight);
 
-    sycl::buffer<int, 2> bufIn(dataIn.get(), dataRange_i);
-    sycl::buffer<int, 2> bufOut(dataOut.get(), dataRange_o);
+    sycl::buffer<int, 2> bufIn(dataIn.data(), dataRange);
+    sycl::buffer<int, 2> bufOut(dataOut.data(), dataRange);
+    sycl::buffer<int, 2> bufOutDeprecated(dataOutDeprecated.data(), dataRange);
 
     auto cmdQueue = util::get_cts_object::queue();
 
     cmdQueue.submit([&](sycl::handler &cgh) {
       auto accIn = bufIn.template get_access<sycl::access_mode::read>(cgh);
-      auto accOut =
-          bufOut.template get_access<sycl::access_mode::write>(cgh);
+      auto accOut = bufOut.template get_access<sycl::access_mode::write>(cgh);
+      auto accOutDeprecated =
+          bufOutDeprecated.template get_access<sycl::access_mode::write>(cgh);
 
-      auto r = sycl::range<2>(nWidth, nHeight);
-      kernel_item_2d kern = kernel_item_2d(accIn, accOut, r);
-
-      cgh.parallel_for(r, kern);
+      kernel_item_2d kern =
+          kernel_item_2d(accIn, accOut, accOutDeprecated, dataRange);
+      cgh.parallel_for(dataRange, kern);
     });
 
     cmdQueue.wait_and_throw();
   }
 
-  if (buffer_verify(dataOut.get(), nWidth, nHeight)) {
-    FAIL(log, "item incorrectly mapped");
-    return false;
-  }
+  // check api call results
+  CHECK(std::reduce(dataOut.begin(), dataOut.end(), true,
+                    std::logical_and<int>{}));
 
-  return true;
+#ifdef SYCL_CTS_ENABLE_DEPRECATED_FEATURES_TESTS
+  // check deprecated api call results
+  CHECK(std::reduce(dataOutDeprecated.begin(), dataOutDeprecated.end(), true,
+                    std::logical_and<int>{}));
+#endif
+
+  STATIC_CHECK_FALSE(std::is_default_constructible_v<sycl::item<2>>);
 }
 
 /** test sycl::device initialization
--- tests/item/item_3d.cpp
+++ tests/item/item_3d.cpp
@@ -3,7 +3,7 @@
 //  SYCL 2020 Conformance Test Suite
 //
 //  Copyright (c) 2017-2022 Codeplay Software LTD. All Rights Reserved.
-//  Copyright (c) 2022 The Khronos Group Inc.
+//  Copyright (c) 2022-2023 The Khronos Group Inc.
 //
 //  Licensed under the Apache License, Version 2.0 (the "License");
 //  you may not use this file except in compliance with the License.
@@ -20,6 +20,9 @@
 *******************************************************************************/
 #include "../common/common.h"
 
+#include <functional>
+#include <numeric>
+
 #define TEST_NAME item_3d
 
 namespace test_item_3d__ {
@@ -27,56 +30,60 @@ using namespace sycl_cts;
 
 class kernel_item_3d {
  protected:
-  typedef sycl::accessor<int, 3, sycl::access_mode::read,
-                             sycl::target::device>
+  typedef sycl::accessor<int, 3, sycl::access_mode::read, sycl::target::device>
       t_readAccess;
-  typedef sycl::accessor<int, 3, sycl::access_mode::write,
-                             sycl::target::device>
+  typedef sycl::accessor<int, 3, sycl::access_mode::write, sycl::target::device>
       t_writeAccess;
 
-  t_readAccess m_x;
-  t_writeAccess m_o;
+  t_readAccess in;
+  t_writeAccess out;
+  t_writeAccess out_deprecated;
   sycl::range<3> r_exp;
   sycl::id<3> offset_exp;
 
  public:
-  kernel_item_3d(t_readAccess in_, t_writeAccess out_, sycl::range<3> r)
-      : m_x(in_), m_o(out_), r_exp(r), offset_exp(sycl::id<3>(0, 0, 0)) {}
+  kernel_item_3d(t_readAccess in_, t_writeAccess out_,
+                 t_writeAccess out_deprecated_, sycl::range<3> r)
+      : in(in_),
+        out(out_),
+        out_deprecated(out_deprecated_),
+        r_exp(r),
+        offset_exp(sycl::id<3>(0, 0, 0)) {}
 
   void operator()(sycl::item<3> item) const {
-    bool result = true;
+    bool all_correct = true;
     sycl::id<3> gid = item.get_id();
 
     size_t dim_a = item.get_id(0) + item.get_id(1) + item.get_id(2);
     size_t dim_b = item[0] + item[1] + item[2];
     size_t id_exp = gid.get(0) + gid.get(1) + gid.get(2);
-    result &= id_exp == dim_a && id_exp == dim_b;
+    all_correct &= id_exp == dim_a && id_exp == dim_b;
 
     sycl::range<3> localRange = item.get_range();
-    result &= localRange == r_exp;
+    all_correct &= localRange == r_exp;
 
-    // TODO: mark this check as testing deprecated functionality
+#ifdef SYCL_CTS_ENABLE_DEPRECATED_FEATURES_TESTS
     sycl::id<3> offset = item.get_offset();
-    result &= offset == offset_exp;
+    out_deprecated[item] = offset == offset_exp;
+#endif
 
     /* get work item range */
     const size_t nWidth = localRange.get(0);
     const size_t nHeight = localRange.get(1);
     const size_t nDepth = localRange.get(2);
-    result &= nWidth == r_exp.get(0) && nHeight == r_exp.get(1) &&
-              nDepth == r_exp.get(2);
+    all_correct &= nWidth == r_exp.get(0) && nHeight == r_exp.get(1) &&
+                   nDepth == r_exp.get(2);
 
     /* find the row major array id for this work item */
     size_t index = gid.get(2) +                     /* z */
                    (gid.get(1) * nWidth) +          /* y */
                    (gid.get(0) * nWidth * nHeight); /* x */
 
-    /* get the global linear id */
+    /* get the global linear id and compare against precomputed index */
     size_t linear_index = item.get_linear_id();
+    all_correct &= in[item] == static_cast<int>(index);
 
-    /* compare against the precomputed index */
-    result &= m_x[item] == static_cast<int>(index);
-    m_o[item] = result;
+    out[item] = all_correct;
   }
 };
 
@@ -92,60 +99,54 @@ void buffer_fill(int *buf, const int nWidth, const int nHeight,
   }
 }
 
-int buffer_verify(int *buf, const int nWidth, const int nHeight,
-                  const int nDepth) {
-  int nErrors = 0;
-  for (int i = 0; i < nWidth * nHeight * nDepth; i++) {
-    nErrors += (buf[i] == 0);
-  }
-  return nErrors;
-}
-
-bool test_item_3d(util::logger &log) {
+void test_item_3d(util::logger &log) {
   const int nWidth = 64;
   const int nHeight = 64;
   const int nDepth = 64;
 
   const int nSize = nWidth * nHeight * nDepth;
 
-  /* allocate host buffers */
-  std::unique_ptr<int> dataIn(new int[nSize]);
-  std::unique_ptr<int> dataOut(new int[nSize]);
+  /* allocate and clear host buffers */
+  std::vector<int> dataIn(nSize, 0);
+  std::vector<int> dataOut(nSize, 0);
+  std::vector<int> dataOutDeprecated(nSize, 0);
 
-  /* clear host buffers */
-  ::memset(dataIn.get(), 0, nSize * sizeof(int));
-  ::memset(dataOut.get(), 0, nSize * sizeof(int));
-
-  buffer_fill(dataIn.get(), nWidth, nHeight, nDepth);
+  buffer_fill(dataIn.data(), nWidth, nHeight, nDepth);
 
   {
     sycl::range<3> dataRange(nWidth, nHeight, nDepth);
 
-    sycl::buffer<int, 3> bufIn(dataIn.get(), dataRange);
-    sycl::buffer<int, 3> bufOut(dataOut.get(), dataRange);
+    sycl::buffer<int, 3> bufIn(dataIn.data(), dataRange);
+    sycl::buffer<int, 3> bufOut(dataOut.data(), dataRange);
+    sycl::buffer<int, 3> bufOutDeprecated(dataOutDeprecated.data(), dataRange);
 
     auto cmdQueue = util::get_cts_object::queue();
 
     cmdQueue.submit([&](sycl::handler &cgh) {
       auto accIn = bufIn.template get_access<sycl::access_mode::read>(cgh);
-      auto accOut =
-          bufOut.template get_access<sycl::access_mode::write>(cgh);
+      auto accOut = bufOut.template get_access<sycl::access_mode::write>(cgh);
+      auto accOutDeprecated =
+          bufOutDeprecated.template get_access<sycl::access_mode::write>(cgh);
 
-      auto r = sycl::range<3>(dataRange);
-      kernel_item_3d kern = kernel_item_3d(accIn, accOut, r);
-
-      cgh.parallel_for(r, kern);
+      kernel_item_3d kern =
+          kernel_item_3d(accIn, accOut, accOutDeprecated, dataRange);
+      cgh.parallel_for(dataRange, kern);
     });
 
     cmdQueue.wait_and_throw();
   }
 
-  if (buffer_verify(dataOut.get(), nWidth, nHeight, nDepth)) {
-    FAIL(log, "item incorrectly mapped");
-    return false;
-  }
+  // check api call results
+  CHECK(std::reduce(dataOut.begin(), dataOut.end(), true,
+                    std::logical_and<int>{}));
 
-  return true;
+#ifdef SYCL_CTS_ENABLE_DEPRECATED_FEATURES_TESTS
+  // check deprecated api call results
+  CHECK(std::reduce(dataOutDeprecated.begin(), dataOutDeprecated.end(), true,
+                    std::logical_and<int>{}));
+#endif
+
+  STATIC_CHECK_FALSE(std::is_default_constructible_v<sycl::item<3>>);
 }
 
 /** test sycl::device initialization
@@ -166,4 +167,4 @@ class TEST_NAME : public util::test_base {
 // construction of this proxy will register the above test
 util::test_proxy<TEST_NAME> proxy;
 
-} /* namespace test_item_2d__ */
+}  // namespace test_item_3d__
