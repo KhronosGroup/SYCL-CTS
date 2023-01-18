79ae61b3e2740e4dfdb9357e93ef7821bcdf2a82
vladimirkhashev <vladimirx.khashev@intel.com>
2023-01-18 07:16:40 -0800

    Update existing kernel tests

--- tests/kernel/kernel_info.cpp
+++ tests/kernel/kernel_info.cpp
@@ -25,90 +25,66 @@
 // ComputeCpp doesn't fully support kernel::get_info of SYCL 2020 spec
 #ifndef SYCL_CTS_COMPILING_WITH_COMPUTECPP
 
-#define TEST_NAME kernel_info
-
 namespace kernel_info__ {
 using namespace sycl_cts;
 
 class kernel0;
 
-/** tests the info for sycl::kernel
- */
-class TEST_NAME : public sycl_cts::util::test_base {
- public:
-  /** return information about this test
+TEST_CASE("kernel info test", "[kernel]") {
+  auto queue = util::get_cts_object::queue();
+  auto deviceList = queue.get_context().get_devices();
+  auto ctx = queue.get_context();
+
+  using k_name = kernel0;
+  auto kb =
+      sycl::get_kernel_bundle<k_name, sycl::bundle_state::executable>(ctx);
+  auto kernel = kb.get_kernel(sycl::get_kernel_id<k_name>());
+
+  queue.submit([&](sycl::handler &cgh) { cgh.single_task<k_name>([=]() {}); });
+
+  /** initialize return values
    */
-  void get_info(test_base::info &out) const override {
-    set_test_info(out, TOSTRING(TEST_NAME), TEST_FILE);
+  cl_uint clUintRet;
+  std::string stringRet;
+  size_t sizeTRet;
+  sycl::range<3> range3Ret{0, 0, 0};
+  cl_ulong clUlongRet;
+
+  // silent warnings
+  (void)clUintRet;
+  (void)sizeTRet;
+  (void)clUlongRet;
+
+  /** check program info parameters
+   */
+  clUintRet = kernel.get_info<sycl::info::kernel::num_args>();
+  stringRet = kernel.get_info<sycl::info::kernel::attributes>();
+  auto dev = util::get_cts_object::device();
+  if (dev.get_info<sycl::info::device::device_type>() ==
+      sycl::info::device_type::custom) {
+    range3Ret =
+        kernel.get_info<sycl::info::kernel_device_specific::global_work_size>(
+            dev);
   }
+  range3Ret = kernel.get_info<
+      sycl::info::kernel_device_specific::compile_work_group_size>(dev);
+  sizeTRet = kernel.get_info<
+      sycl::info::kernel_device_specific::preferred_work_group_size_multiple>(
+      dev);
+  clUlongRet =
+      kernel.get_info<sycl::info::kernel_device_specific::private_mem_size>(
+          dev);
+  sizeTRet =
+      kernel.get_info<sycl::info::kernel_device_specific::work_group_size>(dev);
 
-  /** execute the test
-   */
-  void run(util::logger &log) override {
-    {
-      auto queue = util::get_cts_object::queue();
-      auto deviceList = queue.get_context().get_devices();
-      auto ctx = queue.get_context();
-
-      using k_name = kernel0;
-      auto kb = sycl::get_kernel_bundle<k_name,
-                                        sycl::bundle_state::executable>(ctx);
-      auto kernel = kb.get_kernel(sycl::get_kernel_id<k_name>());
-
-      queue.submit([&](sycl::handler &cgh) {
-        cgh.single_task<k_name>([=]() {});
-      });
-
-      /** initialize return values
-       */
-      cl_uint clUintRet;
-      std::string stringRet;
-      size_t sizeTRet;
-      sycl::range<3> range3Ret{0, 0, 0};
-      cl_ulong clUlongRet;
-
-      // silent warnings
-      (void)clUintRet;
-      (void)sizeTRet;
-      (void)clUlongRet;
-
-      /** check program info parameters
-       */
-      clUintRet = kernel.get_info<sycl::info::kernel::num_args>();
-      stringRet = kernel.get_info<sycl::info::kernel::attributes>();
-      auto dev = util::get_cts_object::device();
-      if (dev.get_info<sycl::info::device::device_type>() ==
-          sycl::info::device_type::custom) {
-        range3Ret =
-            kernel
-                .get_info<sycl::info::kernel_device_specific::global_work_size>(
-                    dev);
-      }
-      range3Ret = kernel.get_info<
-          sycl::info::kernel_device_specific::compile_work_group_size>(dev);
-      sizeTRet = kernel.get_info<sycl::info::kernel_device_specific::
-                                     preferred_work_group_size_multiple>(dev);
-      clUlongRet =
-          kernel.get_info<sycl::info::kernel_device_specific::private_mem_size>(
-              dev);
-      sizeTRet =
-          kernel.get_info<sycl::info::kernel_device_specific::work_group_size>(
-              dev);
-
-      // FIXME: Reenable when struct information descriptors are implemented
+// FIXME: Reenable when struct information descriptors are implemented
 #if !SYCL_CTS_COMPILING_WITH_HIPSYCL && !SYCL_CTS_COMPILING_WITH_COMPUTECPP
-      check_get_info_param<sycl::info::kernel::num_args, uint32_t>(log, kernel);
-      check_get_info_param<sycl::info::kernel::attributes, std::string>(log,
-                                                                        kernel);
+  check_get_info_param<sycl::info::kernel::num_args, uint32_t>(kernel);
+  check_get_info_param<sycl::info::kernel::attributes, std::string>(kernel);
 #endif
 
-      queue.wait_and_throw();
-    }
-  }
+  queue.wait_and_throw();
 };
-
-// register this test with the test_collection
-util::test_proxy<TEST_NAME> proxy;
 
 } /* namespace kernel_info__ */
 #endif // SYCL_CTS_COMPILING_WITH_COMPUTECPP
