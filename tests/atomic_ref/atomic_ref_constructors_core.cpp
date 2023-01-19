d698bd8c8674e576235da7d2562105a28a0c5eaf
vladimirkhashev <vladimirx.khashev@intel.com>
2023-01-19 02:31:36 -0800

    Fix CI

--- tests/atomic_ref/atomic_ref_constructors_core.cpp
+++ tests/atomic_ref/atomic_ref_constructors_core.cpp
@@ -8,13 +8,24 @@
 #include "../common/disabled_for_test_case.h"
 #include "catch2/catch_test_macros.hpp"
 
+// FIXME: re-enable for computecpp when
+// sycl::access::address_space::generic_space and possibility of a SYCL kernel
+// with an unnamed type are implemented in computecpp, re-enable for hipsycl
+// when sycl::info::device::atomic_memory_order_capabilities and
+// sycl::info::device::atomic_memory_scope_capabilities are implemented in
+// hipsycl
+#if !SYCL_CTS_COMPILING_WITH_HIPSYCL && !SYCL_CTS_COMPILING_WITH_COMPUTECPP
+
 #include "atomic_ref_constructors.h"
+
+#endif
 
 namespace atomic_ref_constructors_core {
 
-TEST_CASE("sycl::atomic_ref constructors. core types", "[atomic_ref]") {
+DISABLED_FOR_TEST_CASE(ComputeCpp, hipSYCL)
+("sycl::atomic_ref constructors. core types", "[atomic_ref]")({
   const auto types = atomic_ref_tests_common::get_conformance_type_pack();
   for_all_types<atomic_ref_constructors::run_test>(types);
-}
+});
 
 }  // namespace atomic_ref_constructors_core
--- tests/atomic_ref/atomic_ref_constructors_core_atomic64.cpp
+++ tests/atomic_ref/atomic_ref_constructors_core_atomic64.cpp
@@ -8,11 +8,22 @@
 #include "../common/disabled_for_test_case.h"
 #include "catch2/catch_test_macros.hpp"
 
+// FIXME: re-enable for computecpp when
+// sycl::access::address_space::generic_space and possibility of a SYCL kernel
+// with an unnamed type are implemented in computecpp, re-enable for hipsycl
+// when sycl::info::device::atomic_memory_order_capabilities and
+// sycl::info::device::atomic_memory_scope_capabilities are implemented in
+// hipsycl
+#if !SYCL_CTS_COMPILING_WITH_HIPSYCL && !SYCL_CTS_COMPILING_WITH_COMPUTECPP
+
 #include "atomic_ref_constructors.h"
+
+#endif
 
 namespace atomic_ref_constructors_core_atomic64 {
 
-TEST_CASE("sycl::atomic_ref constructors. atomic64 types", "[atomic_ref]") {
+DISABLED_FOR_TEST_CASE(ComputeCpp, hipSYCL)
+("sycl::atomic_ref constructors. atomic64 types", "[atomic_ref]")({
   auto queue = sycl_cts::util::get_cts_object::queue();
   if (!queue.get_device().has(sycl::aspect::atomic64)) {
     WARN(
@@ -22,6 +33,6 @@ TEST_CASE("sycl::atomic_ref constructors. atomic64 types", "[atomic_ref]") {
   }
   const auto type_pack = atomic_ref_tests_common::get_atomic64_types();
   for_all_types<atomic_ref_constructors::run_test>(type_pack);
-}
+});
 
 }  // namespace atomic_ref_constructors_core_atomic64
