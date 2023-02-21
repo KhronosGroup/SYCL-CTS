575ac6277d22beaf47a5d09e653f9b595ec9c867
Kopylova, NataliaX <nataliax.kopylova@intel.com>
2023-02-21 18:27:33 +0200

    Fix kernel_bundle_semantics

--- tests/kernel_bundle/kernel_bundle_semantics.cpp
+++ tests/kernel_bundle/kernel_bundle_semantics.cpp
@@ -22,6 +22,8 @@
 #include "../common/semantics_reference.h"
 #include "kernel_bundle.h"
 
+struct dummy_kernel;
+
 struct storage {
   bool is_empty;
   std::size_t device_count;
@@ -43,11 +45,13 @@ struct storage {
 
 TEST_CASE("kernel_bundle common reference semantics", "[kernel_bundle]") {
   sycl::context context = sycl_cts::util::get_cts_object::context();
+  sycl::device device = sycl_cts::util::get_cts_object::device();
   sycl::kernel_bundle<sycl::bundle_state::executable> kernel_bundle =
-      sycl::get_kernel_bundle<sycl::bundle_state::executable>(context);
+      sycl::get_kernel_bundle<dummy_kernel, sycl::bundle_state::executable>(
+          context, {device});
   common_reference_semantics::check_host<storage>(kernel_bundle,
                                                   "kernel_bundle");
 
   sycl::queue queue = sycl_cts::util::get_cts_object::queue();
-  sycl_cts::tests::kernel_bundle::define_kernel<class dummy_kernel>(queue);
+  sycl_cts::tests::kernel_bundle::define_kernel<dummy_kernel>(queue);
 }
