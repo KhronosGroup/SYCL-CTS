0739b7d00bf4e0cd5df1ea811b5da35747ac391e
vladimirkhashev <vladimirx.khashev@intel.com>
2023-03-23 09:12:00 -0700

    Apply comments

--- tests/kernel/kernel_info.cpp
+++ tests/kernel/kernel_info.cpp
@@ -51,13 +51,13 @@ TEST_CASE("Test kernel info", "[kernel]") {
 
   {
     /** According to SYCL specification info::kernel::num_args descriptor may
-       only be used to query a kernel that resides in a kernel bundle that was
-       constructed using a backend specific interoperability function or to
-       query a device built-in kernel. In other cases an exception with the
-       errc::invalid error code is thrown. We attampt to use
-       info::kernel::num_args descriptor with user defined kernel which resides
-       in a kernel bundle that was constructed without using interoperability
-       function. So we expect that exception will be thrown.
+        only be used to query a kernel that resides in a kernel bundle that was
+        constructed using a backend specific interoperability function or to
+        query a device built-in kernel. In other cases an exception with the
+        errc::invalid error code is thrown. We attempt to use
+        info::kernel::num_args descriptor with user defined kernel which resides
+        in a kernel bundle that was constructed without using interoperability
+        function. So we expect that exception will be thrown.
      */
     INFO(
         "Check that exception with error code \"errc::invalid\" is thrown in "
@@ -74,12 +74,12 @@ TEST_CASE("Test kernel info", "[kernel]") {
             dev);
   };
   /** According to SYCL specification
-     info::kernel_device_specific::global_work_size descriptor may only be used
-     if the device type is device_type::custom or if the kernel is a built-in
-     kernel. In other cases an exception with the errc::invalid error code is
-     thrown. We attampt to use info::kernel_device_specific::global_work_size
-     descriptor with user defined kernel and expect that exception will be
-     thrown if device type is not custom.
+      info::kernel_device_specific::global_work_size descriptor may only be used
+      if the device type is device_type::custom or if the kernel is a built-in
+      kernel. In other cases an exception with the errc::invalid error code is
+      thrown. We attempt to use info::kernel_device_specific::global_work_size
+      descriptor with user defined kernel and expect that exception will be
+      thrown if device type is not custom.
    */
   if (dev.get_info<sycl::info::device::device_type>() !=
       sycl::info::device_type::custom) {
