0b2af6f42f5c29448e0226fa4ce7ff508674fd3a
Larsen, Steffen <steffen.larsen@intel.com>
2023-04-03 08:19:43 -0700

    Fix image accessor tests
    This commit fixes some mistakes in the image tests.
    
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

--- tests/image/default_image.h
+++ tests/image/default_image.h
@@ -22,7 +22,7 @@
 #define SYCL_CTS_IMAGE_DEFAULT_IMAGE_H
 
 // Enable DPCPP when https://github.com/intel/llvm/issues/8304 been fixed
-#if !(defined(SYCL_CTS_COMPILING_WITH_HIPSYCL) ||    \
+#if !(defined(SYCL_CTS_COMPILING_WITH_HIPSYCL) || \
       defined(SYCL_CTS_COMPILING_WITH_COMPUTECPP) || \
       defined(SYCL_CTS_COMPILING_WITH_DPCPP))
 
@@ -37,9 +37,9 @@ struct default_sampled_image {
     return {
         nullptr, sycl::image_format::r32b32g32a32_sint,
         sycl::image_sampler{sycl::addressing_mode::mirrored_repeat,
-                            sycl::filtering_mode::nearest,
-                            sycl::coordinate_normalization_mode::normalized},
-        sycl::range<1>{}};
+                            sycl::coordinate_normalization_mode::normalized,
+                            sycl::filtering_mode::nearest},
+        sycl::range<1>{1}};
   }
 
   template <sycl::image_target ImageTarget>
@@ -58,13 +58,13 @@ struct default_unsampled_image {
   using data_type = sycl::int4;
   using type = sycl::unsampled_image<1>;
 
-  static void get() {
+  static type get() {
     return {sycl::image_format::r32b32g32a32_sint, sycl::range<1>{1}};
   }
 
   template <sycl::access_mode AccessMode, sycl::image_target ImageTarget>
   using acc_type =
-      sycl::sampled_image_accessor<sycl::int4, 1, AccessMode, ImageTarget>;
+      sycl::unsampled_image_accessor<sycl::int4, 1, AccessMode, ImageTarget>;
 
   template <sycl::access_mode AccessMode, sycl::image_target ImageTarget>
   static acc_type<AccessMode, ImageTarget> get_acc(type &t,
--- tests/image_accessor/generic_image_accessor_semantics.cpp
+++ tests/image_accessor/generic_image_accessor_semantics.cpp
@@ -40,51 +40,58 @@ struct storage_sampled {
   }
 };
 
-TEST_CASE("sampled_image_accessor common reference semantics (host)",
+TEST_CASE("sampled_image_accessor common reference semantics (host_task)",
           "[sampled_image_accessor]") {
   auto sampled_image_0 = default_sampled_image::get();
   auto sampled_image_1 = default_sampled_image::get();
   sycl_cts::util::get_cts_object::queue().submit([&](sycl::handler& cgh) {
     auto sampled_image_accessor_0 =
-        default_sampled_image::get_acc<sycl::image_target::host>(
+        default_sampled_image::get_acc<sycl::image_target::host_task>(
             sampled_image_0, cgh);
     auto sampled_image_accessor_1 =
-        default_sampled_image::get_acc<sycl::image_target::host>(
+        default_sampled_image::get_acc<sycl::image_target::host_task>(
             sampled_image_1, cgh);
-    common_reference_semantics::check_host<storage_sampled>(
-        sampled_image_accessor_0, sampled_image_accessor_1,
-        "sampled_image_accessor<int4, 1, image_target::device>");
-  });
+    cgh.host_task([=] {
+      auto sampled_image_accessor_0_copy = sampled_image_accessor_0;
+      common_reference_semantics::check_host<storage_sampled>(
+          sampled_image_accessor_0_copy, sampled_image_accessor_1,
+          "sampled_image_accessor<int4, 1, image_target::host_task>");
+    });
+  }).wait_and_throw();
 }
 
 TEST_CASE("sampled_image_accessor common reference semantics (kernel)",
           "[sampled_image_accessor]") {
   auto sampled_image = default_sampled_image::get();
   using type = default_sampled_image::acc_type<sycl::image_target::device>;
+  using data_type = default_sampled_image::data_type;
   common_reference_semantics::check_kernel<storage_sampled, type>(
       [&](sycl::handler& cgh) {
-        return sampled_image.get_access(cgh);
-      } "sampled_image_accessor<int4, 1, image_target::device>");
+        return sampled_image.get_access<data_type>(cgh);
+      },
+      "sampled_image_accessor<int4, 1, image_target::device>");
 }
 
 struct storage_unsampled {
   std::size_t size;
 
-  template <typename DataT, int Dimensions, sycl::image_target AccessTarget>
-  explicit storage_unsampled(
-      const sycl::unsampled_image_accessor<DataT, Dimensions, AccessTarget>&
-          unsampled_image_accessor)
+  template <typename DataT, int Dimensions, sycl::access_mode AccessMode,
+            sycl::image_target AccessTarget>
+  explicit storage_unsampled(const sycl::unsampled_image_accessor<
+                             DataT, Dimensions, AccessMode, AccessTarget>&
+                                 unsampled_image_accessor)
       : size(unsampled_image_accessor.size()) {}
 
-  template <typename DataT, int Dimensions, sycl::image_target AccessTarget>
-  bool check(
-      const sycl::unsampled_image_accessor<DataT, Dimensions, AccessTarget>&
-          unsampled_image_accessor) const {
+  template <typename DataT, int Dimensions, sycl::access_mode AccessMode,
+            sycl::image_target AccessTarget>
+  bool check(const sycl::unsampled_image_accessor<DataT, Dimensions, AccessMode,
+                                                  AccessTarget>&
+                 unsampled_image_accessor) const {
     return unsampled_image_accessor.size() == size;
   }
 };
 
-TEST_CASE("unsampled_image_accessor common reference semantics (host)",
+TEST_CASE("unsampled_image_accessor common reference semantics (host_task)",
           "[unsampled_image_accessor]") {
   auto unsampled_image_0 = default_unsampled_image::get();
   auto unsampled_image_1 = default_unsampled_image::get();
@@ -92,17 +99,20 @@ TEST_CASE("unsampled_image_accessor common reference semantics (host)",
     auto unsampled_image_accessor_0 =
         default_unsampled_image::get_acc<sycl::access_mode::read,
                                          sycl::image_target::host_task>(
-            unsampled_image_0);
+            unsampled_image_0, cgh);
 
     auto unsampled_image_accessor_1 =
         default_unsampled_image::get_acc<sycl::access_mode::read,
                                          sycl::image_target::host_task>(
-            unsampled_image_1);
-    common_reference_semantics::check_host<storage_unsampled>(
-        unsampled_image_accessor_0, unsampled_image_accessor_1,
-        "unsampled_image_accessor<int4, 1, access_mode::read, "
-        "image_target::host_task>");
-  });
+            unsampled_image_1, cgh);
+    cgh.host_task([=] {
+      auto unsampled_image_accessor_0_copy = unsampled_image_accessor_0;
+      common_reference_semantics::check_host<storage_unsampled>(
+          unsampled_image_accessor_0_copy, unsampled_image_accessor_1,
+          "unsampled_image_accessor<int4, 1, access_mode::read, "
+          "image_target::host_task>");
+    });
+  }).wait_and_throw();
 }
 
 TEST_CASE("unsampled_image_accessor common reference semantics (kernel)",
@@ -110,9 +120,11 @@ TEST_CASE("unsampled_image_accessor common reference semantics (kernel)",
   auto unsampled_image = default_unsampled_image::get();
   using type = default_unsampled_image::acc_type<sycl::access_mode::read,
                                                  sycl::image_target::device>;
+  using data_type = default_unsampled_image::data_type;
   common_reference_semantics::check_kernel<storage_unsampled, type>(
       [&](sycl::handler& cgh) {
-        return unsampled_image.get_access<sycl::access_mode::read>(cgh);
+        return unsampled_image.get_access<data_type, sycl::access_mode::read>(
+            cgh);
       },
       "unsampled_image_accessor<int4, 1, access_mode::read, "
       "image_target::device>");
