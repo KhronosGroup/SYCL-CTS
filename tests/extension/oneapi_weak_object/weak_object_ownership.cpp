1d08e907496d1efbe7f1d337fbdb8845f47352c2
AmirIpma <amirx.ipmagamabetov@intel.com>
2023-04-03 06:09:39 -0700

    removed host_accessor from ownership tests

--- tests/extension/oneapi_weak_object/weak_object_ownership.cpp
+++ tests/extension/oneapi_weak_object/weak_object_ownership.cpp
@@ -23,12 +23,11 @@
 namespace weak_object_ownership {
 using namespace weak_object_common;
 
-TEST_CASE("weak_object weak_object_ownership", "[weak_object]") {
+TEST_CASE("weak_object_ownership", "[weak_object]") {
 #if !defined SYCL_EXT_ONEAPI_WEAK_OBJECT
   SKIP("SYCL_EXT_ONEAPI_WEAK_OBJECT is not defined");
 #else
   test_weak_object_ownership<sycl::accessor<int> >{}("accessor");
-  // test_weak_object_ownership<sycl::host_accessor<int> >{}("host_accessor");
   test_weak_object_ownership<sycl::buffer<int> >{}("buffer");
   test_weak_object_ownership<sycl::context>{}("context");
   test_weak_object_ownership<sycl::event>{}("event");
@@ -40,4 +39,5 @@ TEST_CASE("weak_object weak_object_ownership", "[weak_object]") {
       .test_local_accessor();
 #endif
 }
+
 }  // namespace weak_object_ownership
