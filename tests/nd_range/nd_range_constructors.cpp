e381db359bd4fcb1f3c0e4bb0bef9ae431f398da
aleks meshkov <alekseix.meshkov@intel.com>
2023-06-19 18:27:25 +0300

    Cover nd_range semantic by-value gaps
    Added checks for copy and move constructors, added tests in kernel
    function and refactored.

--- tests/nd_range/nd_range_constructors.cpp
+++ tests/nd_range/nd_range_constructors.cpp
@@ -20,126 +20,244 @@
 *******************************************************************************/
 
 #include "../common/common.h"
+#include "../common/once_per_unit.h"
 
-#define TEST_NAME nd_range_constructors
-
-namespace TEST_NAMESPACE {
-using namespace sycl_cts;
-
-/**
- * @brief Constructs a default nd_range
- * @tparam dim Number of dimensions of the nd_range
- * @return nd_range<dim> object with default values
- */
-template <int dim>
-inline sycl::nd_range<dim> get_default_nd_range() {
-  const auto range = util::get_cts_object::range<dim>::get(1, 1, 1);
-  return sycl::nd_range<dim>(range, range);
-}
-
-template <int dim>
-void test_nd_range_constructors(util::logger &log, sycl::range<dim> gs,
-                                sycl::range<dim> ls, sycl::id<dim> offset) {
-  sycl::nd_range<dim> no_offset(gs, ls);
-  sycl::nd_range<dim> with_offset(gs, ls, offset);
-
-  {  // Copy assignment, no offset
-    auto defaultRange = get_default_nd_range<dim>();
-    defaultRange = no_offset;
-
-    for (int i = 0; i < dim; i++) {
-      CHECK_VALUE(log, defaultRange.get_global_range()[i], gs[i], i);
-      CHECK_VALUE(log, defaultRange.get_local_range()[i], ls[i], i);
-#if SYCL_CTS_ENABLE_DEPRECATED_FEATURES_TESTS
-      CHECK_VALUE(log, defaultRange.get_offset()[i], (size_t)0, i);
-#endif
-      CHECK_VALUE(log, defaultRange.get_group_range()[i], gs[i] / ls[i], i);
-    }
-  }
-#if SYCL_CTS_ENABLE_DEPRECATED_FEATURES_TESTS
-  {  // Copy assignment, with offset
-    auto defaultRange = get_default_nd_range<dim>();
-    defaultRange = with_offset;
-    for (int i = 0; i < dim; i++) {
-      CHECK_VALUE(log, defaultRange.get_global_range()[i], gs[i], i);
-      CHECK_VALUE(log, defaultRange.get_local_range()[i], ls[i], i);
-      CHECK_VALUE(log, defaultRange.get_offset()[i], offset[i], i);
-      CHECK_VALUE(log, defaultRange.get_group_range()[i], gs[i] / ls[i], i);
-    }
-  }
-#endif
-  {  // Move assignment, no offset
-    auto defaultRange = get_default_nd_range<dim>();
-    defaultRange = std::move(no_offset);
-    for (int i = 0; i < dim; i++) {
-      CHECK_VALUE(log, defaultRange.get_global_range()[i], gs[i], i);
-      CHECK_VALUE(log, defaultRange.get_local_range()[i], ls[i], i);
-#if SYCL_CTS_ENABLE_DEPRECATED_FEATURES_TESTS
-      CHECK_VALUE(log, defaultRange.get_offset()[i], (size_t)0, i);
-#endif
-      CHECK_VALUE(log, defaultRange.get_group_range()[i], gs[i] / ls[i], i);
-    }
-  }
-#if SYCL_CTS_ENABLE_DEPRECATED_FEATURES_TESTS
-  {  // Move assignment, with offset
-    auto defaultRange = get_default_nd_range<dim>();
-    defaultRange = std::move(with_offset);
-    for (int i = 0; i < dim; i++) {
-      CHECK_VALUE(log, with_offset.get_global_range()[i], gs[i], i);
-      CHECK_VALUE(log, with_offset.get_local_range()[i], ls[i], i);
-      CHECK_VALUE(log, with_offset.get_offset()[i], offset[i], i);
-      CHECK_VALUE(log, with_offset.get_group_range()[i], gs[i] / ls[i], i);
-    }
-  }
-#endif
-}
-
-/** test sycl::nd_range initialization
- */
-class TEST_NAME : public util::test_base {
- public:
-  /** return information about this test
-   */
-  void get_info(test_base::info &out) const override {
-    set_test_info(out, TOSTRING(TEST_NAME), TEST_FILE);
-  }
-
-  /** execute the test
-   */
-  void run(util::logger &log) override {
-    constexpr size_t sizes[] = {16, 32, 64};
-
-    {
-      // global size to be set to the size
-      sycl::range<1> gs_1d(sizes[0]);
-      // local size to be set to 1/4 of the sizes
-      sycl::range<1> ls_1d(sizes[0] / 4u);
-      // offset to be set to 1/8 of the sizes
-      sycl::id<1> offset_1d(sizes[0] / 8u);
-      test_nd_range_constructors(log, gs_1d, ls_1d, offset_1d);
-
-      // global size to be set to the size
-      sycl::range<2> gs_2d(sizes[0], sizes[1]);
-      // local size to be set to 1/4 of the sizes
-      sycl::range<2> ls_2d(sizes[0] / 4u, sizes[1] / 4u);
-      // offset to be set to 1/8 of the sizes
-      sycl::range<2> range_2d(sizes[0] / 8u, sizes[1] / 8u);
-      sycl::id<2> offset_2d(range_2d);
-      test_nd_range_constructors(log, gs_2d, ls_2d, offset_2d);
-
-      // global size to be set to the size
-      sycl::range<3> gs_3d(sizes[0], sizes[1], sizes[2]);
-      // local size to be set to 1/4 of the sizes
-      sycl::range<3> ls_3d(sizes[0] / 4, sizes[1] / 4, sizes[2] / 4);
-      // offset to be set to 1/8 of the sizes
-      sycl::range<3> range_3d(sizes[0] / 8u, sizes[1] / 8u, sizes[2] / 8u);
-      sycl::id<3> offset_3d(range_3d);
-      test_nd_range_constructors(log, gs_3d, ls_3d, offset_3d);
-    }
-  }
+namespace nd_range_constructors {
+enum class op_codes : size_t {
+  ctor_range = 0,
+  ctor_copy = 1,
+  ctor_move = 2,
+  assign_copy = 3,
+  assign_move = 4,
+  code_count
 };
 
-// construction of this proxy will register the above test
-util::test_proxy<TEST_NAME> proxy;
+constexpr size_t error_count = to_integral(op_codes::code_count);
 
-}  // namespace TEST_NAMESPACE
+static constexpr size_t sizes[] = {16, 32, 64};
+
+static const std::array<std::string, error_count> error_strings{
+    "nd_range with range was not constructed correctly",
+    "nd_range with nd_range was not constructed correctly",
+    "nd_range with nd_range was not move constructed correctly",
+    "nd_range with nd_range was not copy assigned correctly",
+    "nd_range with nd_range was not move assigned correctly",
+};
+
+template <op_codes Code, typename ResultArray>
+void set_success_operation(ResultArray& result, bool success) {
+  int index = to_integral(Code);
+  result[index] &= success;
+}
+
+std::string get_error_string(int code) { return error_strings[code]; }
+
+template <int dim>
+sycl::id<dim> get_offset() {
+  if constexpr (1 == dim) {
+    sycl::range<1> range(sizes[0] / 8);
+    return sycl::id<1>(range);
+  } else if constexpr (2 == dim) {
+    sycl::range<2> range(sizes[0] / 8, sizes[1] / 8);
+    return sycl::id<2>(range);
+  } else if constexpr (3 == dim) {
+    sycl::range<3> range(sizes[0] / 8, sizes[1] / 8, sizes[2] / 8);
+    return sycl::id<3>(range);
+  }
+}
+
+template <int dim>
+sycl::range<dim> get_local_range() {
+  if constexpr (1 == dim) {
+    return sycl::range<1>(sizes[0] / 4);
+  } else if constexpr (2 == dim) {
+    return sycl::range<2>(sizes[0] / 4, sizes[1] / 4);
+  } else if constexpr (3 == dim) {
+    return sycl::range<3>(sizes[0] / 4, sizes[1] / 4, sizes[2] / 4);
+  }
+}
+
+template <int dim>
+sycl::range<dim> get_global_range() {
+  if constexpr (1 == dim) {
+    return sycl::range<1>(sizes[0]);
+  } else if constexpr (2 == dim) {
+    return sycl::range<2>(sizes[0], sizes[1]);
+  } else if constexpr (3 == dim) {
+    return sycl::range<3>(sizes[0], sizes[1], sizes[2]);
+  }
+}
+
+template <int dim, bool with_offset>
+sycl::nd_range<dim> get_nd_range(sycl::range<dim>& ls, sycl::range<dim>& gs) {
+  if constexpr (with_offset) {
+    return sycl::nd_range<dim>(gs, ls, get_offset<dim>());
+  } else {
+    return sycl::nd_range<dim>(gs, ls);
+  }
+}
+
+template <int dim, bool with_offset, typename ResultArray>
+void check_by_value_semantics(ResultArray& result, sycl::range<dim>& ls,
+                              sycl::range<dim>& gs) {
+  sycl::id<dim> offset = sycl_cts::util::get_cts_object::id<dim>::get(0, 0, 0);
+  if constexpr (with_offset) {
+    offset = get_offset<dim>();
+  }
+  sycl::nd_range<dim> nd_range = get_nd_range<dim, with_offset>(ls, gs);
+  for (int i = 0; i < dim; i++) {
+    set_success_operation<op_codes::ctor_range>(
+        result, nd_range.get_global_range()[i] == gs[i]);
+    set_success_operation<op_codes::ctor_range>(
+        result, nd_range.get_local_range()[i] == ls[i]);
+#if SYCL_CTS_ENABLE_DEPRECATED_FEATURES_TESTS
+    set_success_operation<op_codes::ctor_range>(
+        result, nd_range.get_offset()[i] == offset[i]);
+#endif
+    set_success_operation<op_codes::ctor_range>(
+        result, nd_range.get_group_range()[i] == gs[i] / ls[i]);
+    }
+    sycl::nd_range<dim> copy(nd_range);
+    for (int i = 0; i < dim; i++) {
+      set_success_operation<op_codes::ctor_copy>(
+          result, copy.get_global_range()[i] == gs[i]);
+      set_success_operation<op_codes::ctor_copy>(
+          result, copy.get_local_range()[i] == ls[i]);
+#if SYCL_CTS_ENABLE_DEPRECATED_FEATURES_TESTS
+      set_success_operation<op_codes::ctor_copy>(
+          result, copy.get_offset()[i] == offset[i]);
+#endif
+      set_success_operation<op_codes::ctor_copy>(
+          result, copy.get_group_range()[i] == gs[i] / ls[i]);
+    }
+    sycl::nd_range<dim> copy_assign(gs, ls);
+    copy_assign = copy;
+    for (int i = 0; i < dim; i++) {
+      set_success_operation<op_codes::assign_copy>(
+          result, copy_assign.get_global_range()[i] == gs[i]);
+      set_success_operation<op_codes::assign_copy>(
+          result, copy_assign.get_local_range()[i] == ls[i]);
+#if SYCL_CTS_ENABLE_DEPRECATED_FEATURES_TESTS
+      set_success_operation<op_codes::assign_copy>(
+          result, copy_assign.get_offset()[i] == offset[i]);
+#endif
+      set_success_operation<op_codes::assign_copy>(
+          result, copy_assign.get_group_range()[i] == gs[i] / ls[i]);
+    }
+    sycl::nd_range<dim> move(std::move(copy_assign));
+    for (int i = 0; i < dim; i++) {
+      set_success_operation<op_codes::ctor_move>(
+          result, move.get_global_range()[i] == gs[i]);
+      set_success_operation<op_codes::ctor_move>(
+          result, move.get_local_range()[i] == ls[i]);
+#if SYCL_CTS_ENABLE_DEPRECATED_FEATURES_TESTS
+      set_success_operation<op_codes::ctor_move>(
+          result, move.get_offset()[i] == offset[i]);
+#endif
+      set_success_operation<op_codes::ctor_move>(
+          result, move.get_group_range()[i] == gs[i] / ls[i]);
+    }
+    sycl::nd_range<dim> move_assign(gs, ls);
+    ;
+    move_assign = std::move(move);
+    for (int i = 0; i < dim; i++) {
+      set_success_operation<op_codes::assign_move>(
+          result, move_assign.get_global_range()[i] == gs[i]);
+      set_success_operation<op_codes::assign_move>(
+          result, move_assign.get_local_range()[i] == ls[i]);
+#if SYCL_CTS_ENABLE_DEPRECATED_FEATURES_TESTS
+      set_success_operation<op_codes::assign_move>(
+          result, move_assign.get_offset()[i] == offset[i]);
+#endif
+      set_success_operation<op_codes::assign_move>(
+          result, move_assign.get_group_range()[i] == gs[i] / ls[i]);
+    }
+}
+
+template <int dim, bool with_offset>
+void check_on_host() {
+  bool result[error_count];
+  std::fill(result, result + error_count, true);
+  auto local_range = get_local_range<dim>();
+  auto global_range = get_global_range<dim>();
+  check_by_value_semantics<dim, with_offset>(result, local_range, global_range);
+  for (int i = 0; i < error_count; ++i) {
+    INFO(get_error_string(i));
+    CHECK(result[i]);
+  }
+}
+
+template <int dim, bool with_offset>
+void check_on_device() {
+  bool result[error_count];
+  std::fill(result, result + error_count, true);
+  {
+    sycl::buffer<bool, 1> res_buf(result, sycl::range(error_count));
+    auto queue = once_per_unit::get_queue();
+    queue
+        .submit([&](sycl::handler& cgh) {
+          auto res_acc = res_buf.get_access(cgh);
+          cgh.single_task([=] {
+            auto local_range = get_local_range<dim>();
+            auto global_range = get_global_range<dim>();
+            check_by_value_semantics<dim, with_offset>(res_acc, local_range,
+                                                       global_range);
+          });
+        })
+        .wait_and_throw();
+  }
+  for (int i = 0; i < error_count; ++i) {
+    INFO(get_error_string(i));
+    CHECK(result[i]);
+  }
+}
+
+TEST_CASE("sycl::nd_range constructors. copy by value semantics on host",
+          "[nd_range]") {
+  SECTION("Checking for dim 1 on host without offset") {
+    check_on_host<1, false>();
+  }
+  SECTION("Checking for dim 2 on host without offset") {
+    check_on_host<2, false>();
+  }
+  SECTION("Checking for dim 3 on host without offset") {
+    check_on_host<3, false>();
+  }
+#if SYCL_CTS_ENABLE_DEPRECATED_FEATURES_TESTS
+  SECTION("Checking for dim 1 on host with offset") {
+    check_on_host<1, true>();
+  }
+  SECTION("Checking for dim 2 on host with offset") {
+    check_on_host<2, true>();
+  }
+  SECTION("Checking for dim 3 on host with offset") {
+    check_on_host<3, true>();
+  }
+#endif
+}
+
+TEST_CASE(
+    "sycl::nd_range constructors. copy by value semantics in kernel function",
+    "[nd_range]") {
+  SECTION("Checking for dim 1 in kernel function without offset") {
+    check_on_device<1, false>();
+  }
+  SECTION("Checking for dim 2 in kernel function without offset") {
+    check_on_device<1, false>();
+  }
+  SECTION("Checking for dim 3 in kernel function without offset") {
+    check_on_device<1, false>();
+  }
+#if SYCL_CTS_ENABLE_DEPRECATED_FEATURES_TESTS
+  SECTION("Checking for dim 1 in kernel function with offset") {
+    check_on_device<1, true>();
+  }
+  SECTION("Checking for dim 2 in kernel function with offset") {
+    check_on_device<2, true>();
+  }
+  SECTION("Checking for dim 3 in kernel function with offset") {
+    check_on_device<3, true>();
+  }
+#endif
+}
+}  // namespace nd_range_constructors
--- tests/nd_range/nd_range_equality.cpp
+++ tests/nd_range/nd_range_equality.cpp
@@ -20,147 +20,122 @@
 *******************************************************************************/
 
 #include "../common/common.h"
+#include "../common/once_per_unit.h"
 #include "../common/semantics_by_value.h"
 
 #include <array>
 
-#define TEST_NAME nd_range_equality
-
-namespace {
-
-template <int numDims>
-struct nd_range_equality_kernel;
-
-/**
- * @brief Provides a safe index for checking an operation
- */
-enum class current_check {
-  equal_self,
-  not_equal_self,
-  equal_other_same_global,
-  not_equal_other_same_global,
-  equal_other_same_local,
-  not_equal_other_same_local,
-  equal_other_different,
-  not_equal_other_different,
-  SIZE  // This should be last
-};
-
-struct success_array {
- public:
-  success_array() {
-    std::fill(std::begin(m_rawArray), std::end(m_rawArray), true);
-  }
-  bool& operator[](current_check index) {
-    return m_rawArray[static_cast<size_t>(index)];
-  }
-
- private:
-  std::array<bool, static_cast<size_t>(current_check::SIZE)> m_rawArray;
-};
-
-}  // namespace
-
-namespace TEST_NAME {
+namespace nd_range_equality {
 using namespace sycl_cts;
 
-/** test sycl::device initialization
- */
-class TEST_NAME : public util::test_base {
- public:
-  /** return information about this test
-   */
-  void get_info(test_base::info& out) const final {
-    set_test_info(out, TOSTRING(TEST_NAME), TEST_FILE);
-  }
+constexpr size_t error_count =
+    to_integral(common_by_value_semantics::current_check::size);
 
-  template <int numDims>
-  void test_equality(util::logger& log) {
-    {
-      // Prepare ranges
-      const auto range2 = util::get_cts_object::range<numDims>::get(2, 1, 1);
-      const auto range4 = util::get_cts_object::range<numDims>::get(4, 1, 1);
-      const auto range8 = util::get_cts_object::range<numDims>::get(8, 1, 1);
+template <int numDims>
+std::array<sycl::nd_range<numDims>, 3> get_nd_ranges() {
+  // Prepare ranges
+  const auto range2 = util::get_cts_object::range<numDims>::get(2, 1, 1);
+  const auto range4 = util::get_cts_object::range<numDims>::get(4, 1, 1);
+  const auto range8 = util::get_cts_object::range<numDims>::get(8, 1, 1);
 
-      // Prepare an array of nd_range
-      // The nd_range objects are designed so that:
-      //    0 and 1 have the same global range
-      //    1 and 2 have the same local range
-      //    0 and 2 are completely different
-      using nd_range_t = sycl::nd_range<numDims>;
-      std::array<nd_range_t, 3> objects = {nd_range_t(range8, range4),
-                                           nd_range_t(range8, range2),
-                                           nd_range_t(range4, range2)};
+  // Prepare an array of nd_range
+  // The nd_range objects are designed so that:
+  //    0 and 1 have the same global range
+  //    1 and 2 have the same local range
+  //    0 and 2 are completely different
+  return {sycl::nd_range<numDims>(range8, range4),
+          sycl::nd_range<numDims>(range8, range2),
+          sycl::nd_range<numDims>(range4, range2)};
+}
 
-      // Store comparison results into a success array
-      success_array success;
+template <int numDims>
+void test_equality_on_host() {
+  auto nd_ranges = get_nd_ranges<numDims>();
+  // Perform comparisons on the stored nd_range objects
+  const auto& object0 = nd_ranges[0];
+  const auto& object1 = nd_ranges[1];
+  const auto& object2 = nd_ranges[2];
+  bool result[error_count];
 
-      // Perform comparisons on the stored nd_range objects
-      const auto& object0 = objects[0];
-      const auto& object1 = objects[1];
-      const auto& object2 = objects[2];
-      {
-        auto& currentSuccess = success[current_check::equal_self];
-        currentSuccess = (object0 == object0);
-      }
-      {
-        auto& currentSuccess = success[current_check::not_equal_self];
-        currentSuccess = (object0 != object0);
-      }
-      {
-        auto& currentSuccess = success[current_check::equal_other_same_global];
-        currentSuccess = (object0 == object1);
-      }
-      {
-        auto& currentSuccess =
-            success[current_check::not_equal_other_same_global];
-        currentSuccess = (object0 != object1);
-      }
-      {
-        auto& currentSuccess = success[current_check::equal_other_same_local];
-        currentSuccess = (object1 == object2);
-      }
-      {
-        auto& currentSuccess =
-            success[current_check::not_equal_other_same_local];
-        currentSuccess = (object1 != object2);
-      }
-      {
-        auto& currentSuccess = success[current_check::equal_other_different];
-        currentSuccess = (object0 == object2);
-      }
-      {
-        auto& currentSuccess =
-            success[current_check::not_equal_other_different];
-        currentSuccess = (object0 != object2);
-      }
-
-      // Check nd_range equality operator
-      common_by_value_semantics::check_on_host(
-          log, objects[0], "nd_range " + std::to_string(numDims));
-      CHECK_VALUE(log, success[current_check::equal_self], true, numDims);
-      CHECK_VALUE(log, success[current_check::not_equal_self], false, numDims);
-      CHECK_VALUE(log, success[current_check::equal_other_same_global], false,
-                  numDims);
-      CHECK_VALUE(log, success[current_check::not_equal_other_same_global],
-                  true, numDims);
-      CHECK_VALUE(log, success[current_check::equal_other_same_local], false,
-                  numDims);
-      CHECK_VALUE(log, success[current_check::not_equal_other_same_local], true,
-                  numDims);
+  SECTION(std::string("Checking with same global and dim = ") +
+          std::to_string(numDims)) {
+    common_by_value_semantics::check_equality(object0, object1, result);
+    for (int i = 0; i < error_count; ++i) {
+      INFO(common_by_value_semantics::get_error_string(i));
+      CHECK(result[i]);
     }
   }
-
-  /** execute the test
-   */
-  void run(util::logger& log) final {
-    test_equality<1>(log);
-    test_equality<2>(log);
-    test_equality<3>(log);
+  SECTION(std::string("Checking with same local and dim = ") +
+          std::to_string(numDims)) {
+    common_by_value_semantics::check_equality(object1, object2, result);
+    for (int i = 0; i < error_count; ++i) {
+      INFO(common_by_value_semantics::get_error_string(i));
+      CHECK(result[i]);
+    }
   }
-};
+  SECTION(std::string("Checking with different global and local and dim = ") +
+          std::to_string(numDims)) {
+    common_by_value_semantics::check_equality(object0, object2, result);
+    for (int i = 0; i < error_count; ++i) {
+      INFO(common_by_value_semantics::get_error_string(i));
+      CHECK(result[i]);
+    }
+  }
+}
 
-// construction of this proxy will register the above test
-util::test_proxy<TEST_NAME> proxy;
+template <int numDims>
+void check_equality_on_device(const sycl::nd_range<numDims>& a,
+                              const sycl::nd_range<numDims>& other) {
+  bool result[error_count];
+  {
+    sycl::buffer<bool, 1> res_buf(result, sycl::range(error_count));
+    auto queue = once_per_unit::get_queue();
+    queue
+        .submit([&](sycl::handler& cgh) {
+          auto res_acc = res_buf.get_access(cgh);
+          cgh.single_task([=] {
+            common_by_value_semantics::check_equality(a, other, res_acc);
+          });
+        })
+        .wait_and_throw();
+  }
+  for (int i = 0; i < error_count; ++i) {
+    INFO(common_by_value_semantics::get_error_string(i));
+    CHECK(result[i]);
+  }
+}
 
-}  // namespace TEST_NAME
+template <int numDims>
+void test_equality_on_device() {
+  auto nd_ranges = get_nd_ranges<numDims>();
+  // Perform comparisons on the stored nd_range objects
+  const auto& object0 = nd_ranges[0];
+  const auto& object1 = nd_ranges[1];
+  const auto& object2 = nd_ranges[2];
+  SECTION(std::string("Checking with same global and dim = ") +
+          std::to_string(numDims)) {
+    check_equality_on_device<numDims>(object0, object1);
+  }
+  SECTION(std::string("Checking with same local and dim = ") +
+          std::to_string(numDims)) {
+    check_equality_on_device<numDims>(object1, object2);
+  }
+  SECTION(std::string("Checking with different global and local and dim = ") +
+          std::to_string(numDims)) {
+    check_equality_on_device<numDims>(object0, object2);
+  }
+}
+
+TEST_CASE("Check sycl::nd_range equality check on host", "[nd_range]") {
+  test_equality_on_host<1>();
+  test_equality_on_host<2>();
+  test_equality_on_host<3>();
+}
+
+TEST_CASE("Check sycl::nd_range equality check on device", "[nd_range]") {
+  test_equality_on_device<1>();
+  test_equality_on_device<2>();
+  test_equality_on_device<3>();
+}
+
+}  // namespace nd_range_equality
