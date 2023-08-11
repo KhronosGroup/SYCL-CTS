8833fce9f452a46a0f4c3e9fc7fcebda94f2d1b9
Alexey Bader <alexey.bader@intel.com>
2023-08-11 08:28:04 -0700

    Fix clang-format check.

--- tests/kernel/kernel_attributes_vec_hint.cpp
+++ tests/kernel/kernel_attributes_vec_hint.cpp
@@ -24,7 +24,6 @@
 
 using namespace kernel_attributes;
 
-
 #define RUN_TEST(K_NAME1, K_NAME2, K_NAME3, VEC_T, FUNC1, FUNC2, FUNC3)       \
   {                                                                           \
     auto queue = sycl_cts::util::get_cts_object::queue();                     \
