ab143ddf586d3cc578c792ef68992e003236ce37
Larsen, Steffen <steffen.larsen@intel.com>
2024-01-05 03:16:25 -0800

    Split double * atomic_ref testing
    This commit splits the testing of atomic_ref with double * into separate
    tests to allow for fp64 support checks. This is related to
    https://github.com/KhronosGroup/SYCL-Docs/issues/526.
    
    Signed-off-by: Larsen, Steffen <steffen.larsen@intel.com>

---
+++ tests/atomic_ref/atomic_ref_exchange_test_pointers_fp64.cpp
@@ -0,0 +1,55 @@
+/*******************************************************************************
+//
+//  SYCL 2020 Conformance Test Suite
+//
+//  Copyright (c) 2023 The Khronos Group Inc.
+//
+//  Licensed under the Apache License, Version 2.0 (the "License");
+//  you may not use this file except in compliance with the License.
+//  You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+//  Unless required by applicable law or agreed to in writing, software
+//  distributed under the License is distributed on an "AS IS" BASIS,
+//  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//  See the License for the specific language governing permissions and
+//  limitations under the License.
+//
+//  Provides sycl::atomic_ref::exchange() tests for double *
+//
+*******************************************************************************/
+#include "../common/disabled_for_test_case.h"
+#include "catch2/catch_test_macros.hpp"
+
+#if !SYCL_CTS_COMPILING_WITH_HIPSYCL
+
+#include "atomic_ref_exchange_test.h"
+
+#endif  // !SYCL_CTS_COMPILING_WITH_HIPSYCL
+
+namespace atomic_ref::tests::api::core {
+
+// FIXME: re-enable for hipsycl
+// when sycl::info::device::atomic_memory_order_capabilities and
+// sycl::info::device::atomic_memory_scope_capabilities are implemented in
+// hipsycl
+DISABLED_FOR_TEST_CASE(hipSYCL)
+("sycl::atomic_ref::exchange() test. double *", "[atomic_ref]")({
+  auto queue = sycl_cts::util::get_cts_object::queue();
+  if (!queue.get_device().has(sycl::aspect::fp64)) {
+    SKIP(
+        "Device does not support double precision floating point "
+        "operations.");
+  }
+  const auto type_pack =
+      atomic_ref::tests::common::get_fp64_pointers_type_pack();
+  if (is_64_bits_pointer<void *>() && device_has_not_aspect_atomic64()) {
+    SKIP(
+        "Device does not support atomic64 operations. "
+        "Skipping the test case.");
+  }
+  for_all_types<atomic_ref::tests::api::run_exchange_test>(type_pack);
+});
+
+}  // namespace atomic_ref::tests::api::core
