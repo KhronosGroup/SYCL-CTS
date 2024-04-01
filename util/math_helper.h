f403ea301c81bc4e50094d5e1a5095c57f18caec
Lorenc Bushi <113361374+lbushi25@users.noreply.github.com>
2024-04-01 18:54:04 -0400

    Update math_helper.h

--- util/math_helper.h
+++ util/math_helper.h
@@ -162,7 +162,6 @@ run_func_on_vector_result_ref(funT fun, Args... args) {
   sycl::vec<T, N> res;
   std::map<int, bool> undefined;
   for (int i = 0; i < N; i++) {
-    // bool does not have a default value so we initialize it ourselves to false
     undefined[i] = false;
     resultRef<T> element = fun(getElement(args, i)...);
     if (element.undefined.empty())
