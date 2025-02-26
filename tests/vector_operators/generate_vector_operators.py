e68d31df9c2fe0600de340e351f28269c1da35ce
Andrei Elovikov <andrei.elovikov@intel.com>
2025-02-26 11:48:13 -0800

    Restore relational/logical operators tests for `sycl::vec`
    Seems to have been removed accidentally in
    https://github.com/KhronosGroup/SYCL-CTS/pull/130.

--- tests/vector_operators/generate_vector_operators.py
+++ tests/vector_operators/generate_vector_operators.py
@@ -1355,7 +1355,7 @@ def generate_all_types_specific_return_type_test(type_str, size):
         type=type_str,
         size=str(size),
         swizzle=get_swizzle(size),
-        ret_type=Data.opencl_sized_return_type_dict[type_str],
+        ret_type='rel_log_ret_t<{}>'.format(type_str),
         test_value_1=1,
         test_value_2=2)
     return wrap_with_kernel(
@@ -1419,6 +1419,15 @@ def generate_operator_tests(type_str, input_file, output_file):
                                              type_str, test_str, str(size))
         func_calls += make_func_call(TEST_NAME + '_ALL_TYPES', type_str,
                                      str(size))
+
+        test_str = generate_all_types_specific_return_type_test(
+            type_str, size)
+        test_func_str += wrap_with_test_func(
+            TEST_NAME + '_SPECIFIC_RETURN_TYPES', type_str, test_str,
+            str(size))
+        func_calls += make_func_call(TEST_NAME + '_SPECIFIC_RETURN_TYPES',
+                                        type_str, str(size))
+
         if not type_str in [
                 'float', 'double', 'sycl::half'
         ]:
