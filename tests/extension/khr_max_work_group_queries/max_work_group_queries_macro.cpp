c472bbc2beedf94583adf474659d3380322bd525
Aympab <46692003+Aympab@users.noreply.github.com>
2025-06-11 21:30:22 +0200

    Update max_work_group_queries_macro.cpp
    Co-authored-by: Alexey Bader <alexey.bader@intel.com>

--- tests/extension/khr_max_work_group_queries/max_work_group_queries_macro.cpp
+++ tests/extension/khr_max_work_group_queries/max_work_group_queries_macro.cpp
@@ -23,7 +23,7 @@
 namespace max_num_work_groups_macro::tests {
 TEST_CASE(
     "the max_num_work_groups extension defines the "
-    "SYCL_KHR_MAX_NUM_WORK_GROUPS macro",
+    "SYCL_KHR_MAX_WORK_GROUP_QUERIES macro",
     "[khr_max_num_work_groups]") {
 #ifndef SYCL_KHR_MAX_WORK_GROUP_QUERIES
   static_assert(false, "SYCL_KHR_MAX_WORK_GROUP_QUERIES is not defined");
