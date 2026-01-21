1924c440ee2df7ac664dd4d440273e68c52c055d
Michael Aziz <michael.aziz@intel.com>
2026-01-21 12:26:03 -0800

    Add assertions for  `elementsIn` and `elementsOut`
    Signed-off-by: Michael Aziz <michael.aziz@intel.com>

--- tests/buffer/buffer_api_common.h
+++ tests/buffer/buffer_api_common.h
@@ -62,6 +62,9 @@ template <>
 inline void precalculate<2>(sycl::range<2>& rangeIn, sycl::range<2>& rangeOut,
                             size_t& elementsCount, size_t elementsIn,
                             size_t elementsOut) {
+  assert((elementsIn % 2 == 0) && (elementsOut % 2 == 0) &&
+         "elementsIn and elementsOut must be even "
+         "numbers for 2D buffer precalculation");
   rangeIn = sycl::range<2>(elementsIn / 2, 2);
   rangeOut = sycl::range<2>(elementsOut / 2, 2);
   elementsCount = rangeOut.size();
@@ -71,6 +74,9 @@ template <>
 inline void precalculate<3>(sycl::range<3>& rangeIn, sycl::range<3>& rangeOut,
                             size_t& elementsCount, size_t elementsIn,
                             size_t elementsOut) {
+  assert((elementsIn % 4 == 0) && (elementsOut % 4 == 0) &&
+         "elementsIn and elementsOut must be multiples of 4 "
+         "numbers for 3D buffer precalculation");
   rangeIn = sycl::range<3>(elementsIn / 4, 2, 2);
   rangeOut = sycl::range<3>(elementsOut / 4, 2, 2);
   elementsCount = rangeOut.size();
